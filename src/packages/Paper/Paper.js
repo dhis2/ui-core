809027df295928d46cef7f681d941891979774ec
Hendrik de Graaf <hendrik@dhis2.org>
2018-08-14 17:02:33 +0200

    Removed className and style from Paper

--- src/packages/Paper/Paper.js
+++ src/packages/Paper/Paper.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './paper.css';
 
-const Paper = ({ elevation, children, padded, className, style }) => (
+const Paper = ({ elevation, children, padded }) => (
     <div
-        className={classNames('d2ui-paper', className, `elevation-${elevation}dp`, {
+        className={classNames('d2ui-paper', `elevation-${elevation}dp`, {
             padded: padded,
         })}
-        style={style}
     >
         {children}
     </div>
@@ -16,10 +15,8 @@ const Paper = ({ elevation, children, padded, className, style }) => (
 
 Paper.propTypes = {
     elevation: PropTypes.oneOf([0, 1, 2, 3, 4, 6, 12, 24]),
-    className: PropTypes.string,
     padded: PropTypes.bool,
     children: PropTypes.node,
-    style: PropTypes.object,
 };
 
 Paper.defaultProps = {
