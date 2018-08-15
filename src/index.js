6b7997f347f49854118a210b07441961c38bb998
Hendrik de Graaf <hendrik@dhis2.org>
2018-08-15 14:37:30 +0200

    Changed font family to Rubik as outlined in the dhis2-identity repo: https://github.com/dhis2/dhis2-identity

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'typeface-rubik/index.css';
+import 'material-design-icons/iconfont/material-icons.css';
 import './index.css';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons/iconfont/material-icons.css'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
