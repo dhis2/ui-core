c84bf8ce1c3b1d159495d55e2f871a588fc1d2ad
Viktor Varland <viktor@dhis2.org>
2018-08-20 12:10:21 +0200

    Add prop-types-exact and stripping prop-types out from builds

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,14 @@ const presets = [
 ];
 
 const plugins = [
-    ["@babel/plugin-proposal-class-properties"]
+    ["@babel/plugin-proposal-class-properties"],
+    ['transform-react-remove-prop-types',
+        {
+            removeImport: true,
+            additionalLibraries: ['prop-types-exact'],
+            ignoreFilenames: ["node_modules"]
+        }
+    ]
 ]
 
 module.exports = { presets, plugins };
