4abfd72c9f4a87ca462e89e8d4c4cae5e2c2b275
Viktor Varland <viktor@dhis2.org>
2018-08-21 13:10:28 +0200

    Add UI component

---
+++ src/UI/UI.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bemClassNames } from '../utils';
+
+import '../theme/index.css'
+
+const bem = bemClassNames('d2ui-app');
+
+const UI = ({ theme, direction, children }) => (
+    <div
+        dir={direction}
+        className={bem.b(theme)}
+    >
+        {children}
+    </div>
+);
+
+UI.propTypes = {
+    elevation: PropTypes.oneOf('ltr', 'rtl'),
+    theme: PropTypes.string,
+    children: PropTypes.node,
+};
+
+UI.defaultProps = {
+    direction: 'ltr',
+    theme: 'default',
+};
+
+export default UI;
