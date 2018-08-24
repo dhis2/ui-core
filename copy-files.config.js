8741c7c88ff2e45dfe876d43e783d07805ec8b6b
Viktor Varland <viktor@vardevs.se>
2018-08-24 13:14:57 +0200

    Update build config

--- copy-files.config.js
+++ copy-files.config.js
@@ -2,11 +2,11 @@ module.exports = {
     assets: [
         {
             from: 'node_modules/material-design-icons/iconfont',
-            to: 'dist/theme/fonts/material-design-icons'
+            to: 'build/theme/fonts/material-design-icons'
         },
         {
             from: 'node_modules/typeface-rubik',
-            to: 'dist/theme/fonts/rubik'
+            to: 'build/theme/fonts/rubik'
         }
     ]
 }
