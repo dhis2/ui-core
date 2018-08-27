cc314a4042d4dce8dbf1fe13685d57710c9e54cb
Hendrik de Graaf <hendrik@dhis2.org>
2018-08-27 10:53:28 +0200

    Add throttle helper

---
+++ src/utils/thottle.js
@@ -0,0 +1,21 @@
+export default function(fn, threshhold = 250, scope) {
+    let last;
+    let deferTimer;
+    return function() {
+        var context = scope || this;
+
+        const now = Date.now();
+        const args = arguments;
+        if (last && now < last + threshhold) {
+            // hold on to it
+            clearTimeout(deferTimer);
+            deferTimer = setTimeout(function() {
+                last = now;
+                fn.apply(context, args);
+            }, threshhold);
+        } else {
+            last = now;
+            fn.apply(context, args);
+        }
+    };
+}
