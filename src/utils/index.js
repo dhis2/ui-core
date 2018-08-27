56d6dff459ab916bde8d3fd00896178dbedd032b
Hendrik de Graaf <hendrik@dhis2.org>
2018-08-27 11:01:27 +0200

    Fix typo and import

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export { default as isRtl } from './isRtl';
 export { default as debounce } from './debounce';
+export { default as throttle } from './throttle';
 export { default as easings } from './easings';
 export { default as animatedScrollTo } from './animatedScrollTo';
 export { default as bemClassNames } from './bemClassNames';
