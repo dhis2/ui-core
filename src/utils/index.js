8be11bea92fffdc4c43fdee775476047aab34087
Hendrik de Graaf <hendrik@dhis2.org>
2018-08-29 14:12:36 +0200

    Deleted imports of removed files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,4 @@
 export { default as isRtl } from './isRtl';
-export { default as debounce } from './debounce';
-export { default as throttle } from './throttle';
 export { default as easings } from './easings';
 export { default as animatedScrollTo } from './animatedScrollTo';
 export { default as bemClassNames } from './bemClassNames';
