8092ee798ad3fc2c323365d7f721b62b8482f5fc
Hendrik de Graaf <hendrik@dhis2.org>
2018-09-04 13:02:51 +0200

    Adds a demo of the setDirAndThemeAttributes helper in the example app

--- example-app/src/index.js
+++ example-app/src/index.js
@@ -5,6 +5,14 @@ import './index.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import setDirAndThemeAttributes from 'ui/utils/setDirAndThemeAttributes';
+
+const dummyUserSettings = {
+    keyUiLocale: 'en_US',
+    keyCurrentStyle: 'light_blue/light_blue.css',
+};
+
+setDirAndThemeAttributes(dummyUserSettings);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
