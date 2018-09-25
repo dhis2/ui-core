c6ea8c4d9d17cd827a8f6fde530d0e5ce2164896
Muhammad Adeel <adeel@dhis2.org>
2018-09-25 15:52:20 +0200

    style(HeaderBar) - show empty Apps container on Click

--- src/widgets/HeaderBar/Apps.js
+++ src/widgets/HeaderBar/Apps.js
@@ -1,12 +1,31 @@
 /** @format */
 
 import React from 'react'
+import Paper from '../../core/Paper'
 import Icon from '../../core/Icon'
 
-export default function Apps() {
-    return (
-        <div className="apps-container">
-            <Icon name="apps" />
-        </div>
-    )
+export default class Apps extends React.Component {
+  state = {
+      showPopup: false,
+  }
+
+  togglePopup = () => this.setState({ showPopup: !this.state.showPopup })
+
+  render() {
+      return (
+          <div className="apps-container">
+              <Icon name="apps" onClick={this.togglePopup} />
+              {
+                  this.state.showPopup && (
+                      <div className="contents">
+                          <Paper>
+                              apps list
+                          </Paper>
+                      </div>
+                  )
+              }
+          </div>
+      )
+  }
 }
+
