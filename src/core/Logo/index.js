87e4234366fa0ac2df392adacd39b8225adf5680
Muhammad Adeel <adeelshahid@users.noreply.github.com>
2018-10-02 15:41:33 +0200

    results of procrastination (#55)

--- src/core/Logo/index.js
+++ src/core/Logo/index.js
@@ -3,17 +3,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './styles.css'
 import iconBlue from './svg/icon-blue.svg'
 import iconWhite from './svg/icon-white.svg'
 import logoBlue from './svg/logo-blue.svg'
 import logoWhite from './svg/logo-white.svg'
 
-import classNames from 'classnames/bind'
-import './styles.css'
-import styles from './styles.json'
-
-const cx = classNames.bind(styles)
+import s from './styles'
 
 const images = {
     'icon-blue': iconBlue,
@@ -26,7 +21,7 @@ function Logo({ type, color, width, alt }) {
     const src = images[`${type}-${color}`]
     const style = width ? { width } : null
 
-    return <img alt={alt} src={src} className={`${cx(type)}`} style={style} />
+    return <img alt={alt} src={src} className={s(type)} style={style} />
 }
 
 Logo.propTypes = {
