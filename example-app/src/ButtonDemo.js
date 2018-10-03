380e7994c892ea6846355083aaf90a163435054e
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-03 16:02:56 +0200

    ButtonDemo: space out buttons and dropdowns

--- example-app/src/ButtonDemo.js
+++ example-app/src/ButtonDemo.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { Component } from 'react'
+import React from 'react'
 
 import Paper from 'ui/core/Paper'
 
@@ -90,82 +90,111 @@ function onButtonClick(msg) {
     console.log(msg, 'button clicked')
 }
 
-export default class ProgressDemo extends Component {
-    render() {
-        return (
-            <Paper>
-                <h6>Button components</h6>
-                <br />
-                <PrimaryButton onClick={() => onButtonClick('primary')}>
-                    Primary
-                </PrimaryButton>
-                <FlatButton disabled onClick={() => onButtonClick('flat')}>
-                    Test
-                </FlatButton>
-                <RaisedButton
-                    size="small"
-                    onClick={() => onButtonClick('raised(small)')}
-                >
-                    <Icon name="alarm" /> Raised
-                </RaisedButton>
-                <RaisedButton
-                    size="medium"
-                    onClick={() => onButtonClick('raised(medium)')}
-                >
-                    <Icon name="alarm" />
-                    Raised
-                </RaisedButton>
-                <RaisedButton
-                    size="large"
-                    onClick={() => onButtonClick('raised(large)')}
-                >
-                    <Icon name="alarm" />
-                    Raised
-                </RaisedButton>
-                <RaisedButton onClick={() => onButtonClick('raised')}>
-                    <Icon name="face" />
-                </RaisedButton>
-                <OutlinedButton onClick={() => onButtonClick('outlined')}>
-                    Outlined
-                </OutlinedButton>
-                <CircleButton size="small" onClick={() => onButtonClick()}>
-                    <Icon name="add" />
-                </CircleButton>
-                <CircleButton size="small" onClick={() => onButtonClick()}>
-                    <Icon name="face" />
-                </CircleButton>
-                <CircleButton
-                    aria-labelledby="test"
-                    size="large"
-                    onClick={() => onButtonClick()}
-                >
-                    <Icon name="add" />
-                </CircleButton>
-                <DropdownButton
-                    buttonProps={{
-                        kind: 'primary',
-                        onClick: () => console.log('clicked main btn'),
-                    }}
-                    menuProps={menuProps}
-                    options={dropdownOptions}
-                >
-                    <Icon name="add" />
-                    Dropdown button
-                </DropdownButton>
-                <br />
-                <br />
-                <DropdownButton
-                    buttonProps={{
-                        kind: 'raised',
-                        onClick: () => 'clicked main btn',
-                    }}
-                    menuProps={{ children: menuItems }}
-                    options={dropdownOptions}
-                >
-                    <Icon name="add" />
-                    Second Dropdown
-                </DropdownButton>
-            </Paper>
-        )
-    }
+const HSpace = () => <span style={{ width: 20, display: 'inline-block' }} />
+
+function Buttons() {
+    return (
+        <div style={{ marginBottom: 20 }}>
+            <PrimaryButton onClick={() => onButtonClick('primary')}>
+                Primary
+            </PrimaryButton>
+            <HSpace />
+            <FlatButton disabled onClick={() => onButtonClick('flat')}>
+                Test
+            </FlatButton>
+            <HSpace />
+            <RaisedButton
+                size="small"
+                onClick={() => onButtonClick('raised(small)')}
+            >
+                <Icon name="alarm" /> Raised
+            </RaisedButton>
+            <HSpace />
+            <RaisedButton
+                size="medium"
+                onClick={() => onButtonClick('raised(medium)')}
+            >
+                <Icon name="alarm" />
+                Raised
+            </RaisedButton>
+            <HSpace />
+            <RaisedButton
+                size="large"
+                onClick={() => onButtonClick('raised(large)')}
+            >
+                <Icon name="alarm" />
+                Raised
+            </RaisedButton>
+            <HSpace />
+            <RaisedButton onClick={() => onButtonClick('raised')}>
+                <Icon name="face" />
+            </RaisedButton>
+            <HSpace />
+            <OutlinedButton onClick={() => onButtonClick('outlined')}>
+                Outlined
+            </OutlinedButton>
+            <HSpace />
+            <CircleButton size="small" onClick={() => onButtonClick()}>
+                <Icon name="add" />
+            </CircleButton>
+            <HSpace />
+            <CircleButton size="small" onClick={() => onButtonClick()}>
+                <Icon name="face" />
+            </CircleButton>
+            <HSpace />
+            <CircleButton
+                aria-labelledby="test"
+                size="large"
+                onClick={() => onButtonClick()}
+            >
+                <Icon name="add" />
+            </CircleButton>
+            <HSpace />
+        </div>
+    )
+}
+
+function onDropdownClick(msg) {
+    console.log('onDropdownClick', msg)
+}
+
+function Dropdowns() {
+    return (
+        <div style={{ marginBottom: 20 }}>
+            <DropdownButton
+                buttonProps={{
+                    kind: 'primary',
+                    onClick: () => onDropdownClick('first'),
+                }}
+                menuProps={menuProps}
+                options={dropdownOptions}
+            >
+                <Icon name="add" />
+                Dropdown button
+            </DropdownButton>
+            <HSpace />
+            <DropdownButton
+                buttonProps={{
+                    kind: 'raised',
+                    onClick: () => onDropdownClick('second'),
+                }}
+                menuProps={{ children: menuItems }}
+                options={dropdownOptions}
+            >
+                <Icon name="add" />
+                Second Dropdown
+            </DropdownButton>
+        </div>
+    )
+}
+
+export default function ProgressDemo() {
+    return (
+        <Paper>
+            <h6>Button components</h6>
+            <br />
+            <Buttons />
+            <Dropdowns />
+        </Paper>
+    )
 }
