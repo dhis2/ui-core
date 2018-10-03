a10f22f93b8a0f70ea41d64962d44d3fc8a6d7a3
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-03 14:38:31 +0200

    style(computeHorizontalScrollbarHeight): documentation

--- src/core/Tabs/computeHorizontalScrollbarHeight.js
+++ src/core/Tabs/computeHorizontalScrollbarHeight.js
@@ -4,6 +4,14 @@ import s from './styles'
 
 let horizontalScrollbarHeight
 
+/**
+ * @link https://github.com/material-components/material-components-web/blob/master/packages/mdc-tab-scroller/util.js#L39-L55
+ * Computes the height of browser-rendered horizontal scrollbars using a self-created test element.
+ * May return 0 (e.g. on OS X browsers under default configuration).
+ * @param {!Document} documentObj
+ * @param {boolean=} shouldCacheResult
+ * @return {number}
+ */
 export function computeHorizontalScrollbarHeight() {
     if (horizontalScrollbarHeight) {
         return horizontalScrollbarHeight
