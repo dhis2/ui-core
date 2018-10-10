10dd324481ab8e66e2baeacc9e8fa08527c4a328
Muhammad Adeel <adeel@dhis2.org>
2018-10-10 10:50:58 +0200

    refactor(Menu): default/dense layout

--- src/core/Menu/index.js
+++ src/core/Menu/index.js
@@ -1,5 +1,50 @@
-/** @format */
+import React from 'react'
+import PropTypes from 'prop-types'
+import Paper from '../Paper'
+import MenuItem from './MenuItem'
 
-export * from './Menu'
-export * from './MenuItem'
-export * from './PopoverMenu'
+import s from './styles'
+
+export function Menu({ kind, width, height, list, onClick }) {
+    return (
+        <Paper width={width} height={height}>
+            <ul className={s('ul', kind)}>
+                {list.map(({ label, value, icon, list, type, disabled }, i) => {
+                    if (type === 'divider') {
+                        return <div key={`mid-${i}`} className={s('divider')} />
+                    }
+
+                    return (
+                        <MenuItem
+                            key={`mi-${value}`}
+                            label={label}
+                            value={value}
+                            icon={icon}
+                            list={list}
+                            type={type}
+                            disabled={disabled}
+                            onClick={onClick}
+                        />
+                    )
+                })}
+            </ul>
+        </Paper>
+    )
+}
+
+Menu.defaultProps = {
+    width: 'inherit',
+    height: 'inherit',
+    kind: 'default',
+}
+
+Menu.propTypes = {
+    width: PropTypes.string,
+    height: PropTypes.string,
+    list: PropTypes.array.isRequired,
+    onClick: PropTypes.func.isRequired,
+    kind: PropTypes.oneOf(['default', 'dense']),
+}
+
+export { MenuItem }
+export default Menu
