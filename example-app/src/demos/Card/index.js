9f411ba98c039bf6cdc1cf95318273556925153f
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-12 01:02:59 +0200

    kitchen-sink: Input Text demo

--- example-app/src/demos/Card/index.js
+++ example-app/src/demos/Card/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Card from 'ui/core/Card'
+import { Col, Divider } from '../../helpers'
 
 export class CardDemo extends React.Component {
     static id = 'card'
 
     render() {
-        return <Card width="358px" height="186px" />
+        return (
+            <Col>
+                <Card width="358px" height="186px" />
+                <Divider />
+            </Col>
+        )
     }
 }
