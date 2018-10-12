fed6a9a33a605c79666fd0cce6dbac9bd408a39b
Viktor Varland <viktor@vardevs.se>
2018-10-12 12:19:03 +0200

    docs: ignore docs

---
+++ scripts/build-docs.js
@@ -0,0 +1,35 @@
+const { exec } = require('child_process')
+const fs = require('fs-extra')
+const path = require('path')
+
+async function main() {
+    const root = process.cwd()
+    const exapp = path.join(root, 'example-app')
+    const docs = path.join(root, 'docs')
+
+    exec('npm run build', { cwd: exapp }, async function(
+        error,
+        stdout,
+        stderr
+    ) {
+        if (error) {
+            console.error(`exec error: ${error}`)
+            process.exit(1)
+        }
+
+        console.log(`stdout: ${stdout}`)
+
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+        }
+
+        try {
+            await fs.copy(path.join(exapp, 'build'), docs)
+        } catch (err) {
+            console.error('failed to copy docs', err)
+            process.exit(1)
+        }
+    })
+}
+
+main()
