8ef91e1fd9df7b13724a4b2cf720db9faa08169b
Viktor Varland <viktor@vardevs.se>
2018-10-12 13:30:32 +0200

    chore: format and build docs separately

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -2,11 +2,7 @@ const { exec } = require('child_process')
 const fs = require('fs-extra')
 const path = require('path')
 
-async function main() {
-    const root = process.cwd()
-    const exapp = path.join(root, 'example-app')
-    const docs = path.join(root, 'docs')
-
+function build_docs(exapp, docs) {
     exec('npm run build', { cwd: exapp }, async function(
         error,
         stdout,
@@ -29,6 +25,27 @@ async function main() {
             console.error('failed to copy docs', err)
             process.exit(1)
         }
+    })
+}
+
+async function main() {
+    const root = process.cwd()
+    const exapp = path.join(root, 'example-app')
+    const docs = path.join(root, 'docs')
+
+    exec('npm install', { cwd: exapp }, async function(error, stdout, stderr) {
+        if (error) {
+            console.error(`exec error: ${error}`)
+            process.exit(1)
+        }
+
+        console.log(`stdout: ${stdout}`)
+
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+        }
+
+        build_docs(exapp, docs)
     })
 }
 
