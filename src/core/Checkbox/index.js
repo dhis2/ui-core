d7ecadc5a95f5ee089b10bd5678553ec1a74f21b
Viktor Varland <viktor@vardevs.se>
2018-10-15 23:05:58 +0200

    Checkbox (#93)
    * style(checkbox): checkbox refactor
    
    * fix: remove wrapped expr and remove unused value prop
    
    * fix(checkbox): moved icons out of class, use divider

---
+++ src/core/Checkbox/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Icon from '../Icon'
+
+import { Label, Help } from '../helpers'
+
+import s from './styles'
+
+const icons = {
+    indeterminate: <Icon name="indeterminate_check_box" className="grey" />,
+    unchecked: <Icon name="check_box_outline_blank" className="grey" />,
+    checked: <Icon name="check_box" className="secondary-light" />,
+    disabled: <Icon name="check_box_outline_blank" className="grey-light" />,
+}
+
+class Checkbox extends React.Component {
+    state = {
+        checked: !!this.props.checked,
+        indeterminate: !!this.props.indeterminate,
+    }
+
+    onClick = () => {
+        const checked = !this.state.checked
+        this.props.onChange(this.props.name, checked)
+        this.setState({ checked, indeterminate: false })
+    }
+
+    render() {
+        let icon
+        if (this.state.indeterminate) {
+            icon = icons.indeterminate
+        } else if (this.props.disabled) {
+            icon = icons.disabled
+        } else if (this.state.checked) {
+            icon = icons.checked
+        } else {
+            icon = icons.unchecked
+        }
+
+        return (
+            <label htmlFor={this.props.name} className={s('container')}>
+                <input
+                    type="checkbox"
+                    id={this.props.name}
+                    onClick={this.onClick}
+                    checked={this.state.checked}
+                    disabled={this.props.disabled}
+                />
+                {icon}
+                <span
+                    className={s('label', {
+                        required: this.props.required,
+                    })}
+                >
+                    {this.props.label}
+                </span>
+            </label>
+        )
+    }
+}
+
+Checkbox.defaultProps = {
+    disabled: false,
+    indeterminate: false,
+    checked: false,
+    required: false,
+    label: '',
+}
+
+Checkbox.propTypes = {
+    label: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    indeterminate: PropTypes.bool,
+    required: PropTypes.bool,
+    onChange: PropTypes.func.isRequired,
+    status: PropTypes.oneOf(['default', 'valid', 'warning', 'error']),
+}
+
+export { Checkbox }
+export default Checkbox
