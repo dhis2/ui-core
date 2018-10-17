226407bc373b52725ece33d075b5222d597addc3
Muhammad Adeel <imadeelshahid@gmail.com>
2018-10-17 14:21:58 +0200

    fix(Checkbox)
    - fix color name for from .success -> .valid in theme/blue.css
    - layout changes to Checkbox in Demo
    - add little padding before * when Checkbox / Radio is required

--- docs/src/demos/Atoms/Input/Checkbox/index.js
+++ docs/src/demos/Atoms/Input/Checkbox/index.js
@@ -3,71 +3,102 @@ import Checkbox from 'core/Checkbox'
 
 import { Text, Row, Col, Divider } from '../../../../helpers'
 
+const types = [
+    {
+        name: 'default-checkbox',
+        label: 'Default',
+    },
+    {
+        checked: true,
+        name: 'checked-checkbox',
+        label: 'Checked',
+    },
+    {
+        indeterminate: true,
+        name: 'indeterminate-checkbox',
+        label: 'Indeterminate',
+    },
+    {
+        disabled: true,
+        name: 'disabled-checkbox',
+        label: 'Disabled',
+    },
+]
+
+const states = [
+    {
+        required: true,
+        name: 'required-checkbox',
+        label: 'Required',
+    },
+    {
+        required: true,
+        name: 'valid-checkbox',
+        label: 'Valid',
+        status: 'valid',
+    },
+    {
+        required: true,
+        name: 'warning-checkbox',
+        label: 'Warning',
+        status: 'warning',
+    },
+    {
+        required: true,
+        name: 'error-checkbox',
+        label: 'Error',
+        status: 'error',
+    },
+]
+
+const wrapperStyle = {
+    marginRight: 20,
+    width: 150,
+}
+
 export class CheckboxDemo extends React.Component {
     static id = 'input-checkbox'
 
-    onChange = (target, value) => console.log(`toggle ${target} to ${value}`)
+    state = {
+        'checked-checkbox': true,
+        'valid-checkbox': true,
+    }
+
+    onChange = (name, value) => {
+        this.setState({ [name]: value })
+        console.log(`toggle ${name} to ${value}`)
+    }
 
     render() {
         return (
-            <div>
+            <Col>
+                <Text>Types</Text>
+                <Row style={{ marginBottom: 20 }}>
+                    {types.map(p => (
+                        <div key={`cbr-${p.name}`} style={wrapperStyle}>
+                            <Checkbox
+                                {...p}
+                                checked={this.state[p.name] || false}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                    ))}
+                </Row>
+
+                <Text>States</Text>
                 <Row>
-                    <Col>
-                        <Text>Types</Text>
-                        <Checkbox
-                            label="Checkbox"
-                            name="default-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                        />
-                        <Checkbox
-                            checked
-                            label="Checked Checkbox"
-                            name="checked-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                        />
-                        <Checkbox
-                            indeterminate
-                            name="indeterminate-checkbox"
-                            label="Indeterminate Checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                        />
-                        <Checkbox
-                            disabled
-                            label="Disabled Checkbox"
-                            name="disabled-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                        />
-                    </Col>
-                    <Col>
-                        <Text>States</Text>
-                        <Checkbox
-                            label="Required Checkbox"
-                            name="required-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                            required
-                        />
-                        <Checkbox
-                            label="Valid Checkbox"
-                            name="valid-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                            status="valid"
-                        />
-                        <Checkbox
-                            label="Warning Checkbox"
-                            name="warning-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                            status="warning"
-                        />
-                        <Checkbox
-                            label="Error Checkbox"
-                            name="error-checkbox"
-                            onChange={(name, v) => this.onChange(name, v)}
-                            status="error"
-                        />
-                    </Col>
+                    {states.map(p => (
+                        <div key={`cbr-${p.name}`} style={wrapperStyle}>
+                            <Checkbox
+                                {...p}
+                                checked={this.state[p.name] || false}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                    ))}
                 </Row>
                 <Divider />
-            </div>
+            </Col>
         )
     }
 }
--- src/core/Button/DropdownButton.js
+++ src/core/Button/DropdownButton.js
@@ -66,7 +66,7 @@ class DropdownButton extends Component {
                 {open && (
                     <div className={s('menu')} ref={c => (this.elMenu = c)}>
                         <Menu
-                            width={width}
+                            width={`${width}px`}
                             size={this.props.size}
                             list={this.props.list}
                             onClick={this.props.onClick}
