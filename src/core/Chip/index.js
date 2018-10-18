89abeea1fd61ac3612e0e9b1f04c83775651de68
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2018-10-18 21:14:28 +0200

    Chip with working demo (#98)
    * Chip with working demo
    
    * refactor: simplify css, component
    
    * fix: add hover to static class
    
    * Implemented PR feedback:
    - re-arranged props
    - removed regex to recognise icons string, added `type` prop instead
    - don't destructure this.props.onRemove because it is only used once
    
    * fix: add overflow prop, organise css, apply PR comments
    
    * refactor: use function for special onClick logic
    
    * style(Chip)
    
    - remove duplicate css rule .container.container
    - group/sort CSS props
    - extract return from default else clause
    - remove un-used param from onRemove
    - inline comment for stopPropagation
    - sort className default props first, calculated props seconds
    
    * style(ChipDemo)
    
    - wrap Chips and space them out
    - replace btn- with chip- for Col key

---
+++ src/core/Chip/index.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Icon from '../Icon'
+import s from './styles'
+
+class Chip extends React.PureComponent {
+    onClick = () => {
+        if (!this.props.disabled && this.props.onClick) {
+            return this.props.onClick()
+        }
+    }
+
+    onRemove = evt => {
+        evt.stopPropagation() // stop onRemove from triggering onClick on container
+        this.props.onRemove()
+    }
+
+    showIcon() {
+        const { icon } = this.props
+        if (!icon) {
+            return
+        }
+
+        if (this.props.type === 'image') {
+            return (
+                <img src={icon} alt="chip icon" className={s('image-icon')} />
+            )
+        }
+
+        return <Icon name={icon} className={s('icon')} />
+    }
+
+    showRemove() {
+        if (this.props.onRemove) {
+            return (
+                <Icon
+                    name="cancel"
+                    className={s('remove-icon')}
+                    onClick={this.onRemove}
+                />
+            )
+        }
+    }
+
+    render() {
+        const { label, selected, disabled, dragging, overflow } = this.props
+
+        return (
+            <div
+                className={s('container', {
+                    selected,
+                    disabled,
+                    dragging,
+                    static: !this.props.onClick,
+                })}
+                onClick={this.onClick}
+            >
+                {this.showIcon()}
+                <span className={s('label', { overflow })}>{label}</span>
+                {this.showRemove()}
+            </div>
+        )
+    }
+}
+
+Chip.defaultProps = {
+    type: 'icon',
+    selected: false,
+    disabled: false,
+    dragging: false,
+    overflow: true,
+    onClick: undefined,
+    onRemove: undefined,
+}
+
+Chip.propTypes = {
+    label: PropTypes.string.isRequired,
+    icon: PropTypes.string,
+    type: PropTypes.oneOf(['icon', 'image']),
+    selected: PropTypes.bool,
+    disabled: PropTypes.bool,
+    dragging: PropTypes.bool,
+    overflow: PropTypes.bool,
+    onClick: PropTypes.func,
+    onRemove: PropTypes.func,
+}
+
+export { Chip }
+export default Chip
