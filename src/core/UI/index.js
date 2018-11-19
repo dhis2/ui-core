4aed0d39c287e926e8241e631df64ade5d3d9e5f
Viktor Varland <viktor@vardevs.se>
2018-11-19 21:24:33 +0100

    feat(ui): allow ui to be imported to get fonts/icons without using component

--- src/core/UI/index.js
+++ src/core/UI/index.js
@@ -1,28 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-// import as part of the UI component to make sure that the font is
-// loaded for selective imports which might not import the root index
-// ---
 // note: import css-files with file exts for regexps in e.g. jest/webpack
 import 'typeface-roboto/index.css'
-import 'material-design-icons/iconfont/material-icons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import './base/index.css'
-import './theme/index.css'
-
-function UI({ theme, children }) {
-    return <div className={theme}>{children}</div>
-}
-
-UI.propTypes = {
-    theme: PropTypes.string,
-    children: PropTypes.node,
-}
-
-UI.defaultProps = {
-    theme: 'blue',
-}
-
-export { UI }
-export default UI
