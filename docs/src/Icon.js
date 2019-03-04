ae19b3af52151ada0ae3ce35d8f789d1cb78b97d
Viktor Varland <viktor@vardevs.se>
2019-03-04 21:38:28 +0100

    docs: add proper class name for icon el

--- docs/src/Icon.js
+++ docs/src/Icon.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-function Icon({ name, className, onClick }) {
-    return <i className={className}>{name}</i>
+function Icon({ name }) {
+    return <i className={'material-icons'}>{name}</i>
 }
 
 export { Icon }
