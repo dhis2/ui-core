4178adf6bb605e8694b638debeead0386cdaa7d3
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-03-14 19:22:07 +0100

    refactor: extract creating SelectField & reduce width

--- stories/core/SelectField.js
+++ stories/core/SelectField.js
@@ -9,101 +9,76 @@ const options = [
     { value: '2', label: 'Beware the power of option 2' },
 ]
 
-storiesOf('SelectField', module)
-    .add('With no options', () => (
+const createSelectField = override => (
+    <div style={{ width: '200px' }}>
         <SelectField
             name="without-options"
             label="Without options"
             onChange={noop}
             list={[]}
+            {...override}
         />
-    ))
-    .add('With options', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            list={options}
-        />
-    ))
-    .add('With value', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-        />
-    ))
-    .add('With help', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-            help="This is a help text"
-        />
-    ))
-    .add('With dense size', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-            size="dense"
-        />
-    ))
-    .add('With valid status', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-            status="valid"
-        />
-    ))
-    .add('With warning', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-            status="warning"
-        />
-    ))
-    .add('With error', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-            status="error"
-        />
-    ))
-    .add('Disabled', () => (
-        <SelectField
-            name="with-options"
-            label="With options"
-            onChange={noop}
-            value="0"
-            list={options}
-            disabled={true}
-        />
-    ))
-    .add('With text too long to display it', () => (
-        <div style={{ width: '200px' }}>
-            <SelectField
-                name="with-options"
-                label="With options"
-                onChange={noop}
-                value="2"
-                list={options}
-                disabled={true}
-            />
-        </div>
-    ))
+    </div>
+)
+
+storiesOf('SelectField', module)
+    .add('With no options', () => createSelectField())
+    .add('With options', () =>
+        createSelectField({
+            list: options,
+        })
+    )
+    .add('With value', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+        })
+    )
+    .add('With help text', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+            help: 'This is a help text',
+        })
+    )
+    .add('With size dense', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+            size: 'dense',
+        })
+    )
+    .add('With valid status', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+            status: 'valid',
+        })
+    )
+    .add('With warning', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+            status: 'warning',
+        })
+    )
+    .add('With error', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+            status: 'error',
+        })
+    )
+    .add('With disable true', () =>
+        createSelectField({
+            value: '0',
+            list: options,
+            disabled: true,
+        })
+    )
+    .add('With text too long to display it', () =>
+        createSelectField({
+            value: '2',
+            list: options,
+        })
+    )
