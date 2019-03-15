af03d27f1158b382f051c86f6adf15a2205c30c0
Viktor Varland <viktor@vardevs.se>
2019-03-15 13:29:52 +0100

    docs: update docs to cra2

--- docs/src/App.js
+++ docs/src/App.js
@@ -5,7 +5,7 @@ import Content from './Content'
 
 import 'typeface-roboto/index.css'
 
-import '@dhis2/ui-core/build/es/css/reset.css'
+import { CssReset } from '@dhis2/ui-core'
 
 import './App.css'
 
@@ -15,6 +15,7 @@ import './App.css'
 export default function App() {
     return (
         <div className="container">
+            <CssReset />
             <Sidebar />
             <Content />
         </div>
