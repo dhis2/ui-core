4605c8e9bf12452abc18ee186e34dcb838adffa9
Viktor Varland <viktor@vardevs.se>
2019-03-15 11:51:46 +0100

    chore: update size-limit path

--- .size-limit.js
+++ .size-limit.js
@@ -1,6 +1,10 @@
 module.exports = [
     {
-        path: './build/index.js',
+        path: './build/cjs/index.js',
+        limit: '50 KB',
+    },
+    {
+        path: './build/es/index.js',
         limit: '50 KB',
     },
 ]
