f0963c6238ebf1e16ffe9ce5425a5b9ba3ff26e0
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-05-13 09:38:53 +0800

    refactor: make tree component & story functional

--- src/Tree/index.js
+++ src/Tree/index.js
@@ -3,22 +3,56 @@ import propTypes from 'prop-types'
 import cx from 'classnames'
 import css from 'styled-jsx/css'
 
-export const Label = ({ icon, children }) => (
-    <div className={cx('label')}>{children}</div>
+import { ArrowDown } from '../icons/Arrow'
+
+const Label = ({ icon, children }) => <div>{children}</div>
+
+const Contents = ({ children, open }) => (
+    <div className={cx({ open })}>
+        {children}
+
+        <style jsx>{`
+            div {
+                height: 0;
+                overflow: hidden;
+                flex-grow: 1;
+            }
+
+            .open {
+                height: auto;
+            }
+        `}</style>
+    </div>
 )
 
-const Arrow = ({ hasLeafes, open }) => {
-    const arrowIcon = hasLeafes ? open ? <ArrowUp /> : <ArrowDown /> : null
+const Arrow = ({ hasLeafes, open, onToggleOpen }) => {
+    const arrowIcon = hasLeafes ? <ArrowDown /> : <span />
 
     return (
-        <div className={cx('arrow', { 'has-leafes': hasLeafes })}>
-            {arrowIcon}
+        <div className={cx('arrow', { open, 'has-leafes': hasLeafes })}>
+            <span onClick={onToggleOpen}>{arrowIcon}</span>
+
+            <style jsx>{`
+                div {
+                    width: 24px;
+                }
+
+                span {
+                    display: block;
+                    transform: rotate(-90deg);
+                }
+
+                .open span {
+                    transform: rotate(0);
+                }
+            `}</style>
         </div>
     )
 }
 
-export const Tree = ({ children, open }) => {
-    const hasLeafes = !!children
+const Content = ({ children }) => <div>{children}</div>
+
+export const Tree = ({ children, hasLeafes, open, onToggleOpen }) => {
     const className = cx('tree', {
         open,
         'has-leafes': hasLeafes,
@@ -26,14 +60,27 @@ export const Tree = ({ children, open }) => {
 
     return (
         <div className={className}>
-            <Arrow open={open} hasLeafes={hasLeafes} />
-            {children}
+            <Arrow
+                open={open}
+                hasLeafes={hasLeafes}
+                onToggleOpen={onToggleOpen}
+            />
+            <Content children={children} />
+
+            <style jsx>{`
+                div {
+                    display: flex;
+                }
+            `}</style>
         </div>
     )
 }
 
 Tree.propTypes = {
+    hasLeafes: propTypes.bool,
+    onToggleOpen: propTypes.func,
     open: propTypes.bool,
 }
 
 Tree.Label = Label
+Tree.Contents = Contents
--- stories/Tree.stories.js
+++ stories/Tree.stories.js
@@ -1,16 +1,193 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { State, Store } from '@sambego/storybook-state'
 
 import { Chevron } from '../src/icons/Chevron'
 import { Checkbox } from '../src/Checkbox'
+import { Tree } from '../src/Tree'
 
-const say = something => () => alert(something)
+// Helpers {{{
+const store = new Store(
+    [...Array(50).keys()].reduce(
+        (acc, cur, index) => ({
+            ...acc,
+            [`open${index}`]: false,
+            [`checked${index}`]: false,
+        }),
+        {}
+    )
+)
 
-storiesOf('Tree', module).add('Tree', () => (
-    <Tree>
-        <Tree.Label>
-            <Chevron />
-            <Checkbox label="Tree label" name="l1" onClick={say('l1')} />
-        </Tree.Label>
-    </Tree>
-))
+const toggleOpen = num => () =>
+    store.set({ [`open${num}`]: !store.get(`open${num}`) })
+
+const toggleChecked = num => () =>
+    store.set({ [`checked${num}`]: !store.get(`checked${num}`) })
+// }}} Helpers end
+
+storiesOf('Tree', module)
+    .add('Single root', () => (
+        <State store={store}>
+            {state => (
+                <Tree hasLeafes open={state.open0} onToggleOpen={toggleOpen(0)}>
+                    <Tree.Label open={state.open0}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l1"
+                            onChange={toggleChecked(0)}
+                            checked={state.checked0}
+                        />
+                    </Tree.Label>
+
+                    <Tree.Contents open={state.open0}>
+                        <Tree open={state.open2} onToggleOpen={toggleOpen(2)}>
+                            <Tree.Label open={state.open2}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.1"
+                                    onChange={toggleChecked(2)}
+                                    checked={state.checked2}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree open={state.open3} onToggleOpen={toggleOpen(3)}>
+                            <Tree.Label open={state.open3}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.2"
+                                    onChange={toggleChecked(3)}
+                                    checked={state.checked3}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree open={state.open4} onToggleOpen={toggleOpen(4)}>
+                            <Tree.Label open={state.open4}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.3"
+                                    onChange={toggleChecked(4)}
+                                    checked={state.checked4}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree open={state.open5} onToggleOpen={toggleOpen(5)}>
+                            <Tree.Label open={state.open5}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.4"
+                                    onChange={toggleChecked(5)}
+                                    checked={state.checked5}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                    </Tree.Contents>
+                </Tree>
+            )}
+        </State>
+    ))
+    .add('Multiple roots', () => (
+        <State store={store}>
+            {state => [
+                <Tree hasLeafes open={state.open0} onToggleOpen={toggleOpen(0)}>
+                    <Tree.Label open={state.open0}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l1.1"
+                            onChange={toggleChecked(0)}
+                            checked={state.checked0}
+                        />
+                    </Tree.Label>
+
+                    <Tree.Contents open={state.open0}>
+                        <Tree
+                            hasLeafes
+                            open={state.open2}
+                            onToggleOpen={toggleOpen(2)}
+                        >
+                            <Tree.Label open={state.open2}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.1"
+                                    onChange={toggleChecked(2)}
+                                    checked={state.checked2}
+                                />
+                            </Tree.Label>
+
+                            <Tree.Contents open={state.open2}>
+                                <Tree>
+                                    <Tree.Label>
+                                        <Checkbox
+                                            label="Tree label"
+                                            name="l3.1"
+                                            onChange={toggleChecked(7)}
+                                            checked={state.checked7}
+                                        />
+                                    </Tree.Label>
+                                </Tree>
+                                <Tree>
+                                    <Tree.Label>
+                                        <Checkbox
+                                            label="Tree label"
+                                            name="l3.2"
+                                            onChange={toggleChecked(8)}
+                                            checked={state.checked8}
+                                        />
+                                    </Tree.Label>
+                                </Tree>
+                                <Tree>
+                                    <Tree.Label>
+                                        <Checkbox
+                                            label="Tree label"
+                                            name="l3.3"
+                                            onChange={toggleChecked(9)}
+                                            checked={state.checked9}
+                                        />
+                                    </Tree.Label>
+                                </Tree>
+                            </Tree.Contents>
+                        </Tree>
+                        <Tree open={state.open3} onToggleOpen={toggleOpen(3)}>
+                            <Tree.Label open={state.open3}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.2"
+                                    onChange={toggleChecked(3)}
+                                    checked={state.checked3}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree open={state.open4} onToggleOpen={toggleOpen(4)}>
+                            <Tree.Label open={state.open4}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.3"
+                                    onChange={toggleChecked(4)}
+                                    checked={state.checked4}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree open={state.open5} onToggleOpen={toggleOpen(5)}>
+                            <Tree.Label open={state.open5}>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l2.4"
+                                    onChange={toggleChecked(5)}
+                                    checked={state.checked5}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                    </Tree.Contents>
+                </Tree>,
+                <Tree>
+                    <Tree.Label open={state.open6}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l1.2"
+                            onChange={toggleChecked(6)}
+                            checked={state.checked6}
+                        />
+                    </Tree.Label>
+                </Tree>,
+            ]}
+        </State>
+    ))
