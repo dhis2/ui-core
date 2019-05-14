223774bc2ac0faec7712119c7b1ec0a30c8c5451
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-05-14 09:48:26 +0800

    docs: add two static Tree component stories

--- stories/Tree.stories.js
+++ stories/Tree.stories.js
@@ -6,6 +6,8 @@ import { Chevron } from '../src/icons/Chevron'
 import { Checkbox } from '../src/Checkbox'
 import { Tree } from '../src/Tree'
 
+const say = something => () => alert(something)
+
 // Helpers {{{
 const store = new Store(
     [...Array(50).keys()].reduce(
@@ -26,7 +28,11 @@ const toggleChecked = num => () =>
 // }}} Helpers end
 
 storiesOf('Tree', module)
-    .add('Single root', () => (
+    /**
+     * Stories for development
+     * ============
+     */
+    .add('DX: Single root', () => (
         <State store={store}>
             {state => (
                 <Tree hasLeafes open={state.open0} onToggleOpen={toggleOpen(0)}>
@@ -85,7 +91,7 @@ storiesOf('Tree', module)
             )}
         </State>
     ))
-    .add('Multiple roots', () => (
+    .add('DX: Multiple roots', () => (
         <State store={store}>
             {state => [
                 <Tree hasLeafes open={state.open0} onToggleOpen={toggleOpen(0)}>
@@ -190,4 +196,130 @@ storiesOf('Tree', module)
                 </Tree>,
             ]}
         </State>
+    ))
+
+    /**
+     * Real stories
+     * ============
+     */
+    .add('Multiple roots', () => (
+        <div>
+            <Tree hasLeafes open={false} onToggleOpen={say('toggle tree 1.1')}>
+                <Tree.Label open={false}>
+                    <Checkbox
+                        label="Tree label 1.1"
+                        name="l1.1"
+                        onChange={say('checkbox 1.1 clicked')}
+                        checked={false}
+                    />
+                </Tree.Label>
+
+                <Tree.Contents open={false} />
+            </Tree>
+
+            <Tree hasLeafes open={false} onToggleOpen={say('toggle tree 1.2')}>
+                <Tree.Label open={false}>
+                    <Checkbox
+                        label="Tree label 1.2"
+                        name="l1.2"
+                        onChange={say('checkbox 1.2 clicked')}
+                        checked={false}
+                    />
+                </Tree.Label>
+
+                <Tree.Contents open={false} />
+            </Tree>
+        </div>
+    ))
+    .add('2 Levels open', () => (
+        <Tree hasLeafes open={true} onToggleOpen={say('Toggle tree 1.1')}>
+            <Tree.Label open={true}>
+                <Checkbox
+                    label="Tree label"
+                    name="l1.1"
+                    onChange={say('Check 1.1')}
+                    checked={false}
+                />
+            </Tree.Label>
+
+            <Tree.Contents open={true}>
+                <Tree
+                    hasLeafes
+                    open={true}
+                    onToggleOpen={say('Toggel tree 2.1')}
+                >
+                    <Tree.Label open={true}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l2.1"
+                            onChange={say('Check 2.1')}
+                            checked={false}
+                        />
+                    </Tree.Label>
+
+                    <Tree.Contents open={true}>
+                        <Tree>
+                            <Tree.Label>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l3.1"
+                                    onChange={say('Check 3.1')}
+                                    checked={false}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree>
+                            <Tree.Label>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l3.2"
+                                    onChange={say('Check 3.2')}
+                                    checked={false}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                        <Tree>
+                            <Tree.Label>
+                                <Checkbox
+                                    label="Tree label"
+                                    name="l3.3"
+                                    onChange={say('Check 3.3')}
+                                    checked={false}
+                                />
+                            </Tree.Label>
+                        </Tree>
+                    </Tree.Contents>
+                </Tree>
+                <Tree open={false} onToggleOpen={say('Toggle tree 2.2')}>
+                    <Tree.Label open={false}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l2.2"
+                            onChange={say('Check 2.2')}
+                            checked={false}
+                        />
+                    </Tree.Label>
+                </Tree>
+                <Tree open={false} onToggleOpen={say('Toggle tree 2.3')}>
+                    <Tree.Label open={false}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l2.3"
+                            onChange={say('Check 2.3')}
+                            checked={false}
+                        />
+                    </Tree.Label>
+                </Tree>
+                <Tree open={false} onToggleOpen={say('Toggle tree 2.4')}>
+                    <Tree.Label open={false}>
+                        <Checkbox
+                            label="Tree label"
+                            name="l2.4"
+                            onChange={say('Check 2.4')}
+                            checked={false}
+                        />
+                    </Tree.Label>
+                </Tree>
+            </Tree.Contents>
+        </Tree>
     ))
