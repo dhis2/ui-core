61b55818b33f06fc4497cfaf6b476c08602d631f
Viktor Varland <viktor@dhis2.org>
2019-05-16 14:46:47 +0200

    feat: use a portal to overlay the loader (#223)
    * fix: use a portal to overlay the loader
    
    * fix: use screen/component covers for components
    
    * fix: remove onclick from component cover
    
    * chore: remove log statement
    
    * fix: give screencover a high z-index
    
    * fix: give the componentcover a z-index lower than the dropmenu
    
    * fix: allow screencover to be a pure component
    
    * fix: correct css rule
    
    * fix(screen-cover): set background color on backdrop for better semantics
    
    * fix: add className to covers
    
    * refactor: split into smaller components

---
+++ stories/ScreenCover.stories.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { CircularProgress, ScreenCover, Menu, MenuItem, Divider } from '../src'
+
+const onClick = () => alert('Clicked the backdrop')
+
+storiesOf('ScreenCover', module)
+    .add('Default', () => <ScreenCover onClick={onClick} />)
+
+    .add('CircularProgress', () => (
+        <ScreenCover onClick={onClick}>
+            <CircularProgress />
+        </ScreenCover>
+    ))
+
+    .add('Menu', () => (
+        <ScreenCover onClick={onClick}>
+            <Menu>
+                <MenuItem label="One" value="one">
+                    <Menu>
+                        <MenuItem label="Sub One" value="subone">
+                            <Menu>
+                                <MenuItem label="Sub One-One" value="suboneone">
+                                    <Menu>
+                                        <MenuItem
+                                            label="Sub One-One-One"
+                                            value="suboneoneone"
+                                            onClick={val => {
+                                                alert(`this is ${val}`)
+                                            }}
+                                        />
+                                    </Menu>
+                                </MenuItem>
+                            </Menu>
+                        </MenuItem>
+                    </Menu>
+                </MenuItem>
+
+                <Divider />
+                <MenuItem
+                    label="Two"
+                    value="two"
+                    onClick={val => {
+                        alert(`this is ${val}`)
+                    }}
+                />
+                <MenuItem
+                    label="Three"
+                    value="three"
+                    onClick={val => {
+                        alert(`this is ${val}`)
+                    }}
+                />
+            </Menu>
+        </ScreenCover>
+    ))
