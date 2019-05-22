f502e5aef9e6f23922c862998a07280f04496623
Hendrik de Graaf <hendrik@dhis2.org>
2019-05-22 13:13:37 +0200

    refactor(button-strip): use React.Children.count in prop validation

--- src/ButtonStrip/helper.js
+++ src/ButtonStrip/helper.js
@@ -5,20 +5,18 @@ export const buttonArrayPropType = (props, propName, componentName) => {
     const prop = props[propName]
 
     let error = null
-    let count = 0
+
+    if (React.Children.count(prop) < 2) {
+        error = new Error(`${componentName} should have at least 2 children.`)
+    }
 
     React.Children.forEach(prop, function(child) {
-        count++
         if (child.type !== Button) {
             error = new Error(
                 `${componentName} children should be of type 'Button'.`
             )
         }
     })
-
-    if (count < 2) {
-        error = new Error(`${componentName} should have at least 2 children.`)
-    }
 
     return error
 }
