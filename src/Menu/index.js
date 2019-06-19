0f45ce193385c42806b30d1e0e1b2259c54e2b1b
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-06-19 15:19:22 +0200

    refactor: extract MenuList from Menu component

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,29 +1,24 @@
 import React from 'react'
 import propTypes from 'prop-types'
-import { Card } from '../Card'
 
+import { Card } from '../Card'
+import { MenuList } from '../MenuList'
 import { spacers } from '../theme.js'
 
 const Menu = ({ children, className }) => (
     <Card>
-        <ul className={className}>{children}</ul>
-        <style jsx>{`
-            ul {
-                display: block;
-                position: relative;
-                width: 100%;
-                margin: 0;
+        <div>
+            <MenuList className={className}>{children}</MenuList>
+        </div>
 
+        <style jsx>{`
+            div {
                 padding: ${spacers.dp4} 0;
-                user-select: none;
             }
         `}</style>
     </Card>
 )
 
-Menu.propTypes = {
-    className: propTypes.string,
-    children: propTypes.any.isRequired,
-}
+Menu.propTypes = MenuList.propTypes
 
 export { Menu }
