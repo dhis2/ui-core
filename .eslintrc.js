6734372b2bbe156fa89a82051a56807be21d2d25
Viktor Varland <varl@vlv.io>
2019-07-14 11:26:07 +0200

    fix: update d2-style and fix but in radio button css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,4 @@
-const SEVERITY = 2
-
 module.exports = {
-    root: true,
-
-    extends: 'react-app',
-
     parser: 'babel-eslint',
-
-    env: {
-        browser: true,
-        node: true,
-        jest: true,
-    },
-
-    parserOptions: {
-        // latest standard is ok, eq. to 9
-        ecmaVersion: 2018,
-        ecmaFeatures: {
-            jsx: true,
-            modules: true,
-        },
-    },
-
-    rules: {
-        'max-params': [
-            SEVERITY,
-            {
-                max: 3,
-            },
-        ],
-        'prefer-const': [
-            SEVERITY,
-            {
-                destructuring: 'any',
-                ignoreReadBeforeAssign: false,
-            },
-        ],
-        'no-mixed-spaces-and-tabs': [SEVERITY],
-    },
+    extends: ['./node_modules/@dhis2/cli-style/config/js/eslint.config.js'],
 }
