ad0e1268727e8feea29364e9303c44c96a755a18
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-16 09:52:22 +0200

    fix(tab bar): add missing background color

--- src/Tabs/TabBar.js
+++ src/Tabs/TabBar.js
@@ -16,6 +16,7 @@ const TabBar = ({ fixed, children }) => (
                 box-shadow: inset 0 -1px 0 0 ${colors.grey400};
                 flex-wrap: nowrap;
                 flex-grow: 1;
+                background: ${colors.white};
             }
         `}</style>
     </div>
