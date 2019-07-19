3a663f82298fc886bd93dc5779fbceacec93a3e8
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-07-19 16:03:32 +0200

    fix(switch): make container have a height

--- src/Switch/styles.js
+++ src/Switch/styles.js
@@ -2,6 +2,12 @@ import css from 'styled-jsx/css'
 import { theme, spacers } from '../theme.js'
 
 export default css`
+    label {
+        display: flex;
+        height: 22px;
+        align-items: center;
+    }
+
     input {
         opacity: 0;
         width: 0;
