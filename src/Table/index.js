084aad54e8b2801aae1dd167296ff759ad7d28c9
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-16 12:13:08 +0200

    refactor: move Table root file to src folder

--- src/Table/index.js
+++
@@ -1,8 +0,0 @@
-export { TableBody } from './TableBody'
-export { TableCell } from './TableCell'
-export { TableCellHead } from './TableCellHead'
-export { Table } from './Table'
-export { TableFoot } from './TableFoot'
-export { TableHead } from './TableHead'
-export { TableRow } from './TableRow'
-export { TableRowHead } from './TableRowHead'
