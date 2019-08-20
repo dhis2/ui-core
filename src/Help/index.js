56900fd8be54dde3ee3faa3129de9f8662e7d3f4
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-20 13:11:56 +0200

    fix: help - use margin instead of padding so the correct space is reserved by a parent with box-sizing: border-box - add indent property to toggle margin-left from 0 (false) to dp16 (true | default)

--- src/Help/index.js
+++ src/Help/index.js
@@ -5,12 +5,13 @@ import React from 'react'
 import { statusPropType } from '../common-prop-types'
 import { spacers, theme } from '../theme.js'
 
-const Help = ({ children, valid, error, warning, className }) => (
+const Help = ({ children, valid, error, warning, className, indent }) => (
     <p
         className={cx(className, {
             valid,
             error,
             warning,
+            indent,
         })}
     >
         {children}
@@ -18,13 +19,15 @@ const Help = ({ children, valid, error, warning, className }) => (
         <style jsx>{`
             p {
                 height: 12px;
-                padding-top: ${spacers.dp8};
-                padding-left: ${spacers.dp16};
                 font-size: 12px;
-                margin: 0;
+                margin: ${spacers.dp8} 0 0 0;
                 line-height: 12px;
                 cursor: help;
                 color: ${theme.default};
+            }
+
+            .indent {
+                margin-left: ${spacers.dp16};
             }
 
             .valid {
@@ -42,14 +45,17 @@ const Help = ({ children, valid, error, warning, className }) => (
     </p>
 )
 
-Help.defaultProps = {}
-
 Help.propTypes = {
     className: propTypes.string,
     children: propTypes.string.isRequired,
     error: statusPropType,
     valid: statusPropType,
     warning: statusPropType,
+    indent: propTypes.bool,
+}
+
+Help.defaultProps = {
+    indent: true,
 }
 
 export { Help }
