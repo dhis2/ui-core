c50fcbf25aa2027536ceaad7398002d838380f50
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-22 11:29:29 +0200

    chore: add storybook chapters plugin

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,23 @@
-import { configure, addDecorator } from '@storybook/react'
+import { configure, addDecorator, setAddon } from '@storybook/react'
 import { jsxDecorator } from 'storybook-addon-jsx'
 import { withPropsTable } from 'storybook-addon-react-docgen'
 import { CssResetWrapper } from './css-reset-decorator'
 import { FrameStylesWrapper } from './frame-styles-decorator'
+import chaptersAddon from 'react-storybook-addon-chapters'
 
 import 'typeface-roboto'
 
+// specially added addons
+setAddon(chaptersAddon)
+
+// decorators
 addDecorator(jsxDecorator)
 addDecorator(withPropsTable)
 addDecorator(CssResetWrapper)
 addDecorator(FrameStylesWrapper)
 
 function loadStories() {
-    const req = require.context('../stories', true, /\.stories\.js$/)
+    const req = require.context('../stories', true, /\.(stories|chapters)\.js$/)
     req.keys().forEach(filename => req(filename))
 }
 
