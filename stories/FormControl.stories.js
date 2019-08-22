b8542c69e29fabae1803f97c12f641c3dff7cdbe
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-22 13:21:34 +0200

    fix(form-control): use 2 FormControls in story to show spacing

--- stories/FormControl.stories.js
+++ stories/FormControl.stories.js
@@ -11,14 +11,26 @@ storiesOf('FormControl', module)
         },
     })
     .add('Default', () => (
-        <FormControl>
-            <InputField
-                onChange={() => {
-                    console.log('Nothing happens')
-                }}
-                name="input"
-                label="An input"
-            />
-            <Help>A helpful text</Help>
-        </FormControl>
+        <>
+            <FormControl>
+                <InputField
+                    onChange={() => {
+                        console.log('Nothing happens')
+                    }}
+                    name="input"
+                    label="An input"
+                />
+                <Help>A helpful text</Help>
+            </FormControl>
+            <FormControl>
+                <InputField
+                    onChange={() => {
+                        console.log('Nothing happens')
+                    }}
+                    name="input2"
+                    label="An second input"
+                />
+                <Help>A helpful text</Help>
+            </FormControl>
+        </>
     ))
