24fcae480475d82812c8b87ae0a55d8d3e5489a0
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-27 17:33:32 +0200

    chore(icons): clean up and improve consistency (#395)

--- src/icons/Arrow.js
+++ src/icons/Arrow.js
@@ -1,40 +1,50 @@
 import React from 'react'
-import css from 'styled-jsx/css'
+import propTypes from 'prop-types'
 
-export const styles = css`
-    svg {
-        fill: inherit;
-        height: 24px;
-        width: 24px;
-        vertical-align: middle;
-        pointer-events: none;
-    }
-`
-
-export function ArrowDown() {
+export function ArrowDown({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="48"
             viewBox="0 0 48 48"
+            className={className}
         >
             <path d="M14 20l10 10 10-10z" />
-            <style jsx>{styles}</style>
+            <style jsx>{`
+                svg {
+                    fill: inherit;
+                    height: 24px;
+                    width: 24px;
+                    vertical-align: middle;
+                    pointer-events: none;
+                }
+            `}</style>
         </svg>
     )
 }
+ArrowDown.propTypes = {
+    className: propTypes.string,
+}
 
-export function ArrowUp() {
+export function ArrowUp({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="48"
             viewBox="0 0 48 48"
+            className={className}
         >
             <path d="M14 28l10-10 10 10z" />
-            <style jsx>{styles}</style>
+            <style jsx>{`
+                svg {
+                    fill: inherit;
+                    height: 24px;
+                    width: 24px;
+                    vertical-align: middle;
+                    pointer-events: none;
+                }
+            `}</style>
         </svg>
     )
+}
+ArrowUp.propTypes = {
+    className: propTypes.string,
 }
--- src/icons/Radio.js
+++ src/icons/Radio.js
@@ -1,29 +1,50 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 export function Checked({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="48"
             viewBox="0 0 48 48"
             className={className}
         >
             <path d="M24 14c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm0-10C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 36c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z" />
+            <style jsx>{`
+                svg {
+                    fill: inherit;
+                    height: 24px;
+                    width: 24px;
+                    vertical-align: middle;
+                    pointer-events: none;
+                }
+            `}</style>
         </svg>
     )
+}
+Checked.propTypes = {
+    className: propTypes.string,
 }
 
 export function Unchecked({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="48"
             viewBox="0 0 48 48"
             className={className}
         >
             <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 36c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z" />
+            <style jsx>{`
+                svg {
+                    fill: inherit;
+                    height: 24px;
+                    width: 24px;
+                    vertical-align: middle;
+                    pointer-events: none;
+                }
+            `}</style>
         </svg>
     )
+}
+Unchecked.propTypes = {
+    className: propTypes.string,
 }
