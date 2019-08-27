ac2de2f83aacd82febd384683418852cf5fdaaf0
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-27 22:04:40 +0200

    fix(icons): resolve status icon regressions

--- src/icons/Status.js
+++ src/icons/Status.js
@@ -19,6 +19,12 @@ export function Valid({ className }) {
         >
             <path d="M24 4C12.95 4 4 12.95 4 24c0 11.04 8.95 20 20 20 11.04 0 20-8.96 20-20 0-11.05-8.96-20-20-20zm-4 30L10 24l2.83-2.83L20 28.34l15.17-15.17L38 16 20 34z" />
             <style jsx>{styles}</style>
+            <style jsx>{`
+                svg {
+                    margin-right: ${spacers.dp4};
+                    fill: ${theme.valid};
+                }
+            `}</style>
         </svg>
     )
 }
@@ -38,6 +44,12 @@ export function Warning({ className }) {
         >
             <path d="M2 42h44L24 4 2 42zm24-6h-4v-4h4v4zm0-8h-4v-8h4v8z" />
             <style jsx>{styles}</style>
+            <style jsx>{`
+                svg {
+                    margin-right: ${spacers.dp4};
+                    fill: ${theme.warning};
+                }
+            `}</style>
         </svg>
     )
 }
@@ -57,6 +69,12 @@ export function Error({ className }) {
         >
             <path d="M24 4C12.96 4 4 12.95 4 24s8.96 20 20 20 20-8.95 20-20S35.04 4 24 4zm2 30h-4v-4h4v4zm0-8h-4V14h4v12z" />
             <style jsx>{styles}</style>
+            <style jsx>{`
+                svg {
+                    margin-right: ${spacers.dp4};
+                    fill: ${theme.error};
+                }
+            `}</style>
         </svg>
     )
 }
@@ -76,6 +94,12 @@ export function Info({ className }) {
         >
             <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm2 30h-4V22h4v12zm0-16h-4v-4h4v4z" />
             <style jsx>{styles}</style>
+            <style jsx>{`
+                svg {
+                    margin-right: ${spacers.dp4};
+                    fill: ${theme.info};
+                }
+            `}</style>
         </svg>
     )
 }
@@ -90,7 +114,7 @@ export function Loading({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
+            viewBox="22 22 44 44"
             className={className}
         >
             <circle
