0f96558e4405b16e1caa1c1e97a00f3217891070
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2019-08-27 13:49:22 +0200

    refactor(fieldset): simplify the component and narrow scope (#393)

--- src/FieldSet/index.js
+++ src/FieldSet/index.js
@@ -1,35 +1,12 @@
-import React, { Children, cloneElement } from 'react'
+import React from 'react'
 import propTypes from 'prop-types'
 import cx from 'classnames'
-import { statusPropType } from '../common-prop-types'
 import styles from './styles'
 
-const FieldSet = ({
-    className,
-    children,
-    stacked,
-    label,
-    valid,
-    warning,
-    error,
-    required,
-}) => (
+const FieldSet = ({ className, children, label }) => (
     <fieldset className={cx(className)}>
-        {label && (
-            <legend className={cx({ valid, warning, error, required })}>
-                {label}
-            </legend>
-        )}
-        <div className={cx({ stacked })}>
-            {Children.map(children, child =>
-                cloneElement(child, {
-                    className: cx(child.props.className, 'fieldset-input'),
-                    valid,
-                    warning,
-                    error,
-                })
-            )}
-        </div>
+        {label && <legend>{label}</legend>}
+        <div>{children}</div>
         <style jsx>{styles}</style>
     </fieldset>
 )
@@ -37,12 +14,7 @@ const FieldSet = ({
 FieldSet.propTypes = {
     className: propTypes.string,
     children: propTypes.arrayOf(propTypes.element).isRequired,
-    stacked: propTypes.bool,
     label: propTypes.string,
-    required: propTypes.bool,
-    valid: statusPropType,
-    warning: statusPropType,
-    error: statusPropType,
 }
 
 export { FieldSet }
--- src/FieldSet/styles.js
+++ src/FieldSet/styles.js
@@ -8,42 +8,10 @@ export default css`
         margin: 0;
         padding: 0;
     }
+
     legend {
         font-size: 16px;
         color: ${theme.default};
-        margin: 0 0 ${spacers.dp12};
-    }
-
-    legend.required::after {
-        padding-left: ${spacers.dp4};
-        content: '*';
-    }
-
-    legend.valid {
-        color: ${theme.valid};
-    }
-
-    legend.warning {
-        color: ${theme.warning};
-    }
-
-    legend.error {
-        color: ${theme.error};
-    }
-    div {
-        display: flex;
-        flex-direction: row;
-    }
-
-    div.stacked {
-        flex-direction: column;
-    }
-
-    div > :global(.fieldset-input) {
-        margin-right: ${spacers.dp12};
-    }
-
-    div.stacked > :global(.fieldset-input) {
-        margin-bottom: ${spacers.dp8};
+        margin: ${spacers.dp12} 0 ${spacers.dp12};
     }
 `
