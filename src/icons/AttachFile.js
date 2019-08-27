744d1624efb925eedb9fdcfde2a72f6625fe0fac
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-27 17:33:32 +0200

    chore(icons): clean up and improve consistency (#395)

--- src/icons/AttachFile.js
+++ src/icons/AttachFile.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-export function Close({ className }) {
+export function AttachFile({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 48"
             className={className}
         >
-            <path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z" />
+            <path d="M33 12v23c0 4.42-3.58 8-8 8s-8-3.58-8-8V10c0-2.76 2.24-5 5-5s5 2.24 5 5v21c0 1.1-.89 2-2 2-1.11 0-2-.9-2-2V12h-3v19c0 2.76 2.24 5 5 5s5-2.24 5-5V10c0-4.42-3.58-8-8-8s-8 3.58-8 8v25c0 6.08 4.93 11 11 11s11-4.92 11-11V12h-3z" />
             <style jsx>{`
                 svg {
                     fill: inherit;
@@ -21,6 +21,7 @@ export function Close({ className }) {
         </svg>
     )
 }
-Close.propTypes = {
+
+AttachFile.propTypes = {
     className: propTypes.string,
 }
