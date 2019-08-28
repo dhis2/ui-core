f7c8f474a8f055f4c2b393ca825294b0dda61ce6
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-28 09:34:55 +0200

    fix(icons): remove duplicate fill style declarations

--- src/icons/Status.js
+++ src/icons/Status.js
@@ -18,7 +18,6 @@ export function Valid({ className }) {
             <path d="M24 4C12.95 4 4 12.95 4 24c0 11.04 8.95 20 20 20 11.04 0 20-8.96 20-20 0-11.05-8.96-20-20-20zm-4 30L10 24l2.83-2.83L20 28.34l15.17-15.17L38 16 20 34z" />
             <style jsx>{`
                 svg {
-                    fill: inherit;
                     height: 24px;
                     width: 24px;
                     vertical-align: middle;
@@ -49,7 +48,6 @@ export function Warning({ className }) {
             <path d="M2 42h44L24 4 2 42zm24-6h-4v-4h4v4zm0-8h-4v-8h4v8z" />
             <style jsx>{`
                 svg {
-                    fill: inherit;
                     height: 24px;
                     width: 24px;
                     vertical-align: middle;
@@ -80,7 +78,6 @@ export function Error({ className }) {
             <path d="M24 4C12.96 4 4 12.95 4 24s8.96 20 20 20 20-8.95 20-20S35.04 4 24 4zm2 30h-4v-4h4v4zm0-8h-4V14h4v12z" />
             <style jsx>{`
                 svg {
-                    fill: inherit;
                     height: 24px;
                     width: 24px;
                     vertical-align: middle;
@@ -111,7 +108,6 @@ export function Info({ className }) {
             <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm2 30h-4V22h4v12zm0-16h-4v-4h4v4z" />
             <style jsx>{`
                 svg {
-                    fill: inherit;
                     height: 24px;
                     width: 24px;
                     vertical-align: middle;
