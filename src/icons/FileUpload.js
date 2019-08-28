379811d1a2987700b453e3d57b0ca8d6c91f9e83
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2019-08-28 11:15:19 +0200

    chore(icons): clean up and improve consistency (#395)
    * chore(icons): clean up and improve consistency
    
    * fix(icons): resolve status icon regressions
    
    * fix(icons): restore Indeterminate viewBox to 24 instead of 48
    
    * refactor(icons): remove shared styles and props but keep consistency
    
    * fix(icons): remove duplicate fill style declarations

--- src/icons/FileUpload.js
+++ src/icons/FileUpload.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-export function Close({ className }) {
+export function FileUpload({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 48"
             className={className}
         >
-            <path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z" />
+            <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
             <style jsx>{`
                 svg {
                     fill: inherit;
@@ -21,6 +21,7 @@ export function Close({ className }) {
         </svg>
     )
 }
-Close.propTypes = {
+
+FileUpload.propTypes = {
     className: propTypes.string,
 }
