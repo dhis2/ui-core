cfe93286b1edcabb7f90de569023b724b72310e9
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-29 14:16:49 +0200

    refactor: file-input - Rename from FileField to FileInput - Split up into several name-spaced components to avoid a lot of text props and improve composability

---
+++ src/FileInput/Placeholder.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import { colors, spacers } from '../theme'
+
+const Placeholder = ({ children }) => (
+    <p>
+        {children}
+        <style jsx>{`
+            p {
+                margin: 0;
+                padding-top: ${spacers.dp8};
+                font-size: 14px;
+                color: ${colors.grey700};
+            }
+        `}</style>
+    </p>
+)
+
+Placeholder.propTypes = {
+    children: propTypes.string,
+}
+
+export { Placeholder }
