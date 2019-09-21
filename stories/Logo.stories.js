07354558eb52c30d4a148ca9f68493cca85125f8
ismay <ismay@ismaywolff.nl>
2019-09-21 10:25:43 +0200

    ci: lint source on ci as well (#435)
    * ci: lint source on ci as well
    
    * chore: add missing prop-types
    
    * chore: ignore prop-type warnings for inline storybook components
    
    * chore: remove unused imports
    
    * fix: use onFocus and onBlur from props
    
    * chore: allow numbers as well for checkbox label
    
    * chore: ignore prop-type warnings for CssVariables story
    
    * docs: add jsdoc comments

--- stories/Logo.stories.js
+++ stories/Logo.stories.js
@@ -4,6 +4,7 @@ import { Logo, LogoWhite, LogoIcon, LogoIconWhite } from '../src'
 
 const Wrapper = fn => <div style={{ width: '358px' }}>{fn()}</div>
 
+// eslint-disable-next-line react/prop-types
 const Background = ({ children }) => (
     <div style={{ backgroundColor: '#276696' }}>{children}</div>
 )
