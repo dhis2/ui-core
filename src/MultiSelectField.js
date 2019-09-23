5a855e2e361fb57bbd0b74d9c3b064ea65a4b9bb
ismay <ismay@ismaywolff.nl>
2019-09-23 16:42:53 +0200

    refactor(select): update component to new design
    To match the design of the Select in the Design System, the Select
    component based on the native HTML select has to be deprecated and a
    custom select component must be used.
    
    This also adds components for multi-select and filterable select menus.
    
    BREAKING CHANGE: Select deprecated in favor of the SingleSelect, refer
    to the docs for the API.
    
    BREAKING CHANGE: SelectField deprecated in favor of SingleSelectField,
    see the docs for the API.
    
    BREAKING CHANGE: The new Select components do not accept a standard HTML `option`
    element, instead they require use of SingleSelectOption, MultiSelectOption,
    or a component with a matching API.
    
    BREAKING CHANGE: The Select's `onChange` no longer returns a standard `Event.target`,
    instead returns the selected Object with properties: `label` and
    `string`, or an array of those for the MultiSelects
    
    BREAKING CHANGE: The Selects all require a selected prop, instead of a value.
    Where selected is an object with a label and a value string, or an array of
    those for the MultiSelect.
    
    BREAKING CHANGE: Since the Selects do not use a native input element, the name
    prop has been dropped from the Select components.

---
+++ src/MultiSelectField.js
@@ -0,0 +1,170 @@
+import propTypes from '@dhis2/prop-types'
+import React from 'react'
+
+import { statusPropType, multiSelectedPropType } from './common-prop-types.js'
+
+import { Field } from './Field.js'
+import { Label } from './Label.js'
+import { Help } from './Help.js'
+import { MultiSelect } from './MultiSelect.js'
+
+/**
+ * @module
+ *
+ * @param {MultiSelectField.PropTypes} props
+ * @returns {React.Component}
+ *
+ * @example import { MultiSelectField } from '@dhis2/ui-core'
+ *
+ * @see Specification: {@link https://github.com/dhis2/design-system/blob/master/molecules/select.md|Design system}
+ * @see Live demo: {@link /demo/?path=/story/multiselectfield--default|Storybook}
+ */
+class MultiSelectField extends React.Component {
+    render() {
+        const {
+            className,
+            onChange,
+            onFocus,
+            onBlur,
+            required,
+            label,
+            valid,
+            error,
+            disabled,
+            warning,
+            loading,
+            selected,
+            tabIndex,
+            helpText,
+            validationText,
+            maxHeight,
+            children,
+            clearable,
+            clearText,
+            filterable,
+            filterPlaceholder,
+            placeholder,
+            prefix,
+            empty,
+            loadingText,
+            noMatchText,
+            initialFocus,
+            dense,
+        } = this.props
+
+        return (
+            <Field className={className}>
+                {label && (
+                    <Label required={required} disabled={disabled}>
+                        {label}
+                    </Label>
+                )}
+
+                <MultiSelect
+                    selected={selected}
+                    tabIndex={tabIndex}
+                    maxHeight={maxHeight}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    loading={loading}
+                    error={error}
+                    warning={warning}
+                    valid={valid}
+                    disabled={disabled}
+                    clearable={clearable}
+                    clearText={clearText}
+                    filterable={filterable}
+                    filterPlaceholder={filterPlaceholder}
+                    placeholder={placeholder}
+                    prefix={prefix}
+                    empty={empty}
+                    loadingText={loadingText}
+                    noMatchText={noMatchText}
+                    initialFocus={initialFocus}
+                    dense={dense}
+                >
+                    {children}
+                </MultiSelect>
+
+                {helpText && <Help>{helpText}</Help>}
+
+                {validationText && (
+                    <Help error={error} warning={warning} valid={valid}>
+                        {validationText}
+                    </Help>
+                )}
+            </Field>
+        )
+    }
+}
+
+MultiSelectField.defaultProps = {
+    selected: [],
+}
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ *
+ * @prop {function} onChange
+ * @prop {string} label
+ * @prop {Array} selected
+ * @prop {string} [className]
+ * @prop {string} [tabIndex]
+ * @prop {Array|Object} [children]
+ * @prop {boolean} [required]
+ * @prop {boolean} [disabled]
+ * @prop {boolean} [dense]
+ * @prop {boolean} [valid] - `valid`, `warning`, `error`, `loading`, are mutually exclusive
+ * @prop {boolean} [warning]
+ * @prop {boolean} [error]
+ * @prop {boolean} [loading]
+ * @prop {function} [onFocus]
+ * @prop {function} [onBlur]
+ * @prop {boolean} [initialFocus]
+ * @prop {string} [validationText]
+ * @prop {string} [helpText]
+ * @prop {string} clearText
+ * @prop {boolean} [clearable]
+ * @prop {Array|Object} empty
+ * @prop {string} filterPlaceholder
+ * @prop {boolean} [filterable]
+ * @prop {string} [loadingText]
+ * @prop {string} [maxHeight]
+ * @prop {string} noMatchText
+ * @prop {string} [placeholder]
+ * @prop {string} [prefix]
+ */
+MultiSelectField.propTypes = {
+    onChange: propTypes.func.isRequired,
+    label: propTypes.string,
+    selected: multiSelectedPropType.isRequired,
+    className: propTypes.string,
+    tabIndex: propTypes.string,
+    children: propTypes.node,
+    required: propTypes.bool,
+    disabled: propTypes.bool,
+    dense: propTypes.bool,
+    valid: statusPropType,
+    warning: statusPropType,
+    error: statusPropType,
+    loading: propTypes.bool,
+    onFocus: propTypes.func,
+    onBlur: propTypes.func,
+    initialFocus: propTypes.bool,
+    validationText: propTypes.string,
+    helpText: propTypes.string,
+    clearText: propTypes.string.isRequired,
+    clearable: propTypes.bool,
+    empty: propTypes.node.isRequired,
+    filterPlaceholder: propTypes.string.isRequired,
+    filterable: propTypes.bool,
+    loadingText: propTypes.string,
+    maxHeight: propTypes.string,
+    noMatchText: propTypes.string.isRequired,
+    placeholder: propTypes.string,
+    prefix: propTypes.string,
+}
+
+export { MultiSelectField }
---
+++ src/Select/InputPrefix.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import { colors, spacers } from '../theme.js'
+
+const InputPrefix = ({ prefix, className }) => {
+    if (!prefix) {
+        return null
+    }
+
+    return (
+        <div className={className}>
+            {prefix}
+
+            <style jsx>{`
+                div {
+                    color: ${colors.grey600};
+                    padding-right: ${spacers.dp4};
+                    font-size: 14px;
+                    user-select: none;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+InputPrefix.propTypes = {
+    className: propTypes.string,
+    prefix: propTypes.string,
+}
+
+export { InputPrefix }
