bee86ae2a6e872de2aada775710136d1de320338
ismay <ismay@ismaywolff.nl>
2019-09-23 16:42:53 +0200

    refactor(select): update component to new design

---
+++ src/Option.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import cx from 'classnames'
+import css from 'styled-jsx/css'
+
+import { ChevronRight } from './icons/Chevron.js'
+
+import styles from './Option/styles.js'
+
+const subChevron = css.resolve`
+    svg {
+        margin: 0 -14px 0 auto;
+        width: 18px;
+        height: 18px;
+        pointer-events: none;
+        user-select: none;
+    }
+`
+
+const subMenu = css.resolve`
+    div {
+        display: none;
+        position: absolute;
+        top: 0;
+        left: 100%;
+        white-space: nowrap;
+    }
+
+    li:hover > div {
+        display: block;
+    }
+`
+
+const SubMenu = ({ children, className }) => (
+    <div className={className}>{children}</div>
+)
+
+SubMenu.propTypes = {
+    className: propTypes.string,
+    children: propTypes.element,
+}
+
+const createOnClickHandler = (onClick, value) => evt => {
+    if (onClick) {
+        evt.preventDefault()
+        evt.stopPropagation()
+        onClick(value)
+    }
+}
+
+/**
+ * @module
+ * @param {Option.PropTypes}
+ * @returns {React.Component}
+ *
+ * @example import { Option } from '@dhis2/ui-core'
+ *
+ * @see Specification: {@link https://github.com/dhis2/design-system/blob/master/molecules/menu.md|Design system}
+ * @see Live demo: {@link /demo/?path=/story/menu--default|Storybook}
+ */
+const Option = ({
+    href,
+    value,
+    label,
+    icon,
+    children,
+    active,
+    disabled,
+    dense,
+    onClick,
+    className,
+}) => {
+    const hasMenu = !!children
+    const isClickable = href || onClick
+    const LinkElement = isClickable ? 'a' : 'span'
+    const linkElementProps = {}
+
+    if (isClickable) {
+        linkElementProps.href = href
+        linkElementProps.onClick = createOnClickHandler(onClick, value)
+    }
+
+    return (
+        <li
+            className={cx(className, subMenu.className, {
+                disabled,
+                dense,
+                active,
+            })}
+        >
+            <LinkElement className="link" {...linkElementProps}>
+                {icon}
+                <div className="label">{label}</div>
+
+                {hasMenu && <ChevronRight className={subChevron.className} />}
+                {subChevron.styles}
+
+                {hasMenu && (
+                    <SubMenu className={subMenu.className}>{children}</SubMenu>
+                )}
+                {subMenu.styles}
+            </LinkElement>
+
+            <style jsx>{styles}</style>
+        </li>
+    )
+}
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ *
+ * @prop {string|Node} label
+ * @prop {string} [value]
+ * @prop {string} [href]
+ * @prop {function} [onClick] - Click handler called with `value` as the sole argument
+ * @prop {string} [className]
+ * @prop {Element} [children]
+ * @prop {Element} [icon]
+ * @prop {boolean} [dense]
+ * @prop {boolean} [active]
+ * @prop {boolean} [disabled]
+ */
+Option.propTypes = {
+    label: propTypes.oneOfType([propTypes.string, propTypes.node]).isRequired,
+
+    value: propTypes.string,
+    href: propTypes.string,
+    onClick: propTypes.func,
+
+    className: propTypes.string,
+    children: propTypes.element,
+    icon: propTypes.element,
+
+    dense: propTypes.bool,
+    active: propTypes.bool,
+    disabled: propTypes.bool,
+}
+
+export { Option }
--- src/Select.js
+++ src/Select.js
@@ -1,238 +1,114 @@
+import React, { Component } from 'react'
 import propTypes from '@dhis2/prop-types'
-import React, { Component, createRef } from 'react'
-import css from 'styled-jsx/css'
-import cx from 'classnames'
-
-import { statusPropType } from './common-prop-types.js'
-import { theme, colors, spacers } from './theme.js'
-
-import { ArrowDown } from './icons/Arrow.js'
-import { StatusIcon } from './icons/Status.js'
-
-const TailIcon = () => (
-    <div>
-        <ArrowDown />
-
-        <style jsx>{`
-            div {
-                pointer-events: none;
-                position: absolute;
-                right: 4px;
-            }
-        `}</style>
-    </div>
-)
-
-const styles = css`
-    select {
-        background: none;
-        border: 0;
-        color: ${colors.grey900};
-        font-size: inherit;
-        height: 100%;
-        outline: 0;
-        /** 15px => 16px inner spacing - 1px of the border**/
-        padding: 0 12px 0 15px;
-        width: 100%;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-    }
-
-    .select {
-        align-items: center;
-        box-sizing: border-box;
-        display: flex;
-        height: 42px;
-        left: 1px;
-        position: relative;
-        border: 1px solid ${colors.grey500};
-        border-radius: 3px;
-        box-shadow: inset 0 0 0 1px rgba(102, 113, 123, 0.15),
-            inset 0 1px 2px 0 rgba(102, 113, 123, 0.1);
-        font-size: 14px;
-    }
-
-    option:not(:checked) {
-        color: black;
-    }
-
-    select:-moz-focusring {
-        color: transparent;
-        text-shadow: 0 0 0 #000;
-    }
-
-    .select.dense {
-        height: 34px;
-    }
-
-    .select.focus {
-        border-color: ${colors.teal400};
-    }
-
-    .select.valid {
-        border-color: ${theme.valid};
-    }
-
-    .select.warning {
-        border-color: ${theme.warning};
-    }
-
-    .select.error {
-        border-color: ${theme.error};
-    }
-
-    .disabled,
-    .disabled-select {
-        border-color: ${theme.disabled};
-        background-color: ${colors.grey100};
-        color: ${theme.disabled};
-        cursor: not-allowed;
-    }
-
-    .status-icon {
-        flex-shrink: 0;
-        width: 24px;
-        height: 24px;
-        margin-right: ${spacers.dp4};
-        margin-left: ${spacers.dp12};
-    }
-
-    .status-icon:empty {
-        display: none;
-    }
-
-    .status-icon:last-child {
-        margin-right: ${spacers.dp32};
-    }
-`
+import { ArrowDown, ArrowUp } from './icons/Arrow.js'
+import { colors } from './theme.js'
+import { SelectMenu } from './SelectMenu.js'
 
 export class Select extends Component {
-    selectRef = createRef()
-
     state = {
-        focus: this.props.initialFocus,
+        open: false,
     }
 
-    onFocus = e => {
-        this.setState({ focus: true })
+    containerRef = React.createRef()
 
+    componentDidMount() {
+        document.addEventListener('click', this.handleOutsideClick)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleOutsideClick)
+    }
+
+    handleOpen = () => {
+        this.setState({ open: true })
+    }
+
+    handleClose = () => {
+        this.setState({ open: false })
+    }
+
+    handleFocus = e => {
         if (this.props.onFocus) {
             this.props.onFocus(e)
         }
+
+        this.handleOpen()
     }
 
-    onBlur = e => {
-        this.setState({ focus: false })
-
-        if (this.props.onBlur) {
-            this.props.onBlur(e)
-        }
+    handleOptionClick = value => {
+        this.props.onChange(value)
+        this.handleClose()
     }
 
-    isFocused() {
-        return this.state.focus
-    }
+    handleOutsideClick = e => {
+        const hasRef = this.containerRef.current
+        const isInsideClick = hasRef && this.containerRef.current.contains(e.target)
 
-    componentDidMount() {
-        if (this.props.focus) {
-            this.selectRef.current.focus()
+        if (!isInsideClick) {
+            this.handleClose()
         }
     }
 
     render() {
-        const {
-            dense,
-            disabled,
-            onChange,
-            value,
-            children,
-            name,
-            tabIndex,
-            valid,
-            warning,
-            error,
-            loading,
-            className,
-        } = this.props
-
-        const { focus } = this.state
+        const { children, tabIndex, placeholder } = this.props
+        const Arrow = this.state.open ? ArrowUp : ArrowDown
 
         return (
-            <div
-                className={cx('select', className, {
-                    dense,
-                    disabled,
-                    focus,
-                    valid,
-                    warning,
-                    error,
-                })}
-            >
-                <select
-                    onChange={onChange}
-                    value={value || -1}
-                    disabled={disabled}
-                    onFocus={this.onFocus}
-                    onBlur={this.onBlur}
-                    ref={this.selectRef}
-                    name={name}
+            <div className="container" ref={this.containerRef}>
+                <div
+                    className="input"
                     tabIndex={tabIndex}
+                    onFocus={this.handleFocus}
                 >
-                    <option
-                        key="hidden-default-value"
-                        hidden
-                        disabled
-                        value="-1"
-                    />
-                    {children}
-                </select>
-
-                <div className="status-icon">
-                    <TailIcon />
-
-                    <StatusIcon
-                        error={error}
-                        valid={valid}
-                        loading={loading}
-                        warning={warning}
-                    />
+                    <div>{placeholder}</div>
+                    <Arrow className="arrow" />
                 </div>
-                <style jsx>{styles}</style>
+                {this.state.open && (
+                    <SelectMenu onOptionClick={this.handleOptionClick}>
+                        {children}
+                    </SelectMenu>
+                )}
+
+                <style jsx>{`
+                    .container {
+                        position: relative;
+                    }
+
+                    .input {
+                        border: 1px solid ${colors.grey500};
+                        display: flex;
+                        align-items: center;
+                        padding: 0 10px;
+                        height: 42px;
+                        border-radius: 3px;
+                        font-size: 14px;
+                        line-height: 16px;
+                        box-shadow: inset 0 0 0 1px rgba(102, 113, 123, 0.15),
+                            inset 0 1px 2px 0 rgba(102, 113, 123, 0.1);
+                    }
+
+                    .arrow {
+                        margin-left: auto;
+                    }
+                `}</style>
             </div>
         )
     }
 }
 
+Select.defaultProps = {
+    tabIndex: 0,
+    onFocus: () => {},
+    onBlur: () => {},
+    placeholder: '',
+}
+
 Select.propTypes = {
-    className: propTypes.string,
-
-    name: propTypes.string.isRequired,
     onChange: propTypes.func.isRequired,
-
-    value: propTypes.string,
-    tabIndex: propTypes.string,
-
+    value: propTypes.string.isRequired,
+    children: propTypes.node.isRequired,
+    tabIndex: propTypes.number,
     onFocus: propTypes.func,
     onBlur: propTypes.func,
-
-    children: propTypes.oneOfType([
-        propTypes.arrayOf(
-            propTypes.shape({
-                tagName: propTypes.oneOf(['OPTION', 'OPTGROUP']),
-            })
-        ),
-        propTypes.shape({
-            tagName: propTypes.oneOf(['OPTION', 'OPTGROUP']),
-        }),
-    ]),
-
-    disabled: propTypes.bool,
-    dense: propTypes.bool,
-    focus: propTypes.bool,
-    initialFocus: propTypes.bool,
-
-    valid: statusPropType,
-    warning: statusPropType,
-    error: statusPropType,
-    loading: propTypes.bool,
+    placeholder: propTypes.string,
 }
