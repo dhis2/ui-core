9233e20e413361429be6812738a3b0e4edf51807
Viktor Varland <varl@vlv.io>
2019-09-23 09:56:46 +0200

    refactor(tablecell,tablecellhead): use class to avoid dynamic styles

--- src/Table/TableCell.js
+++ src/Table/TableCell.js
@@ -1,26 +1,28 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import propTypes from 'prop-types'
 import css from 'styled-jsx/css'
+import cx from 'classnames'
 
 const tableCellStyles = css`
     td {
         border-bottom: 1px solid #e8edf2;
         font-size: 14px;
         line-height: 18px;
+        padding: 13px 12px;
+        height: 45px;
+    }
+
+    .dense {
+        padding: 9px 12px;
+        height: 36px;
     }
 `
 
 export const TableCell = ({ children, colSpan, rowSpan, dense }) => (
-    <td colSpan={colSpan} rowSpan={rowSpan}>
+    <td colSpan={colSpan} rowSpan={rowSpan} className={cx({ dense })}>
         {children}
 
         <style jsx>{tableCellStyles}</style>
-        <style jsx>{`
-            td {
-                padding: ${dense ? '9px 12px' : '13px 12px'};
-                height: ${dense ? '36px' : '45px'};
-            }
-        `}</style>
     </td>
 )
 
--- src/Table/TableCellHead.js
+++ src/Table/TableCellHead.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import css from 'styled-jsx/css'
+import cx from 'classnames'
 
 const tableCellHeadStyles = css`
     th {
         border-bottom: 1px solid #e8edf2;
         font-size: 14px;
         line-height: 18px;
+        padding: 13px 12px;
+        height: 45px;
+    }
+
+    .dense {
+        padding: 9px 12px;
+        height: 36px;
     }
 `
 
 export const TableCellHead = ({ children, colSpan, rowSpan, dense }) => (
-    <th colSpan={colSpan} rowSpan={rowSpan}>
+    <th colSpan={colSpan} rowSpan={rowSpan} className={cx({ dense })}>
         {children}
 
         <style jsx>{tableCellHeadStyles}</style>
-        <style jsx>{`
-            th {
-                padding: ${dense ? '9px 12px' : '13px 12px'};
-                height: ${dense ? '36px' : '45px'};
-            }
-        `}</style>
     </th>
 )
 
