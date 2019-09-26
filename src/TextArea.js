3d5a04d65c5800cf236375da28cbcfba9e203f35
Hendrik de Graaf <hendrik@dhis2.org>
2019-09-26 17:08:12 +0200

    docs(textarea): add jsDocs to TextArea

--- src/TextArea.js
+++ src/TextArea.js
@@ -185,6 +185,36 @@ TextArea.defaultProps = {
     onFocus: () => {},
 }
 
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ *
+ * @prop {string} name
+ * @prop {function} onChange
+ * @prop {function} [onBlur=() => {}]
+ * @prop {function} [onFocus=() => {}]
+ * @prop {string} [className]
+ * @prop {string} [placeholder]
+ * @prop {string} [value]
+ * @prop {string} [tabIndex]
+ *
+ * @prop {boolean} [disabled]
+ * @prop {boolean} [dense] - Compact mode
+ * @prop {boolean} [initialFocus]
+ *
+ * @prop {boolean} [valid] - `valid`, `warning`, `error`, and `loading`
+ * are mutually exclusive
+ * @prop {boolean} [warning]
+ * @prop {boolean} [error]
+ * @prop {boolean} [loading]
+ *
+ * @prop {boolean} [autoGrow]
+ * @prop {boolean} [readOnly]
+ * @prop {string} [resize=vertical] - rezise attribute on the textarea,
+ * one of `none`, `both`, `horizontal`, `vertical`
+ * @prop {number} [rows=4]
+ */
+
 TextArea.propTypes = {
     name: propTypes.string.isRequired,
 
