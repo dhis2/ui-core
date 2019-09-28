da8c3bd991cc8667ba3a93af3ae3300dce785f54
Viktor Varland <varl@vlv.io>
2019-09-28 10:33:47 +0200

    refactor: implement the Radio component consistently with other inputs
    The Radio component now only contains a single radio-input with label, and supports a `dense` prop. A new RadioGroup component was introduced that contains a set of radios that toggle. The new RadioGroupField component contains a label, radio-group, help-text and validation-text, wrapped in a field.
    
    HISTORY:
    
    * refactor(radio): remove redundant styles
    * refactor(radio): remove focus state and style on input :focus selector
    * fix(radio): adjust disabled SVG fill color to match design-specs
    * fix(radio): remove stories that contain unsupported props
    * refactor(radio): move styles into component and remove Radio directory
    * refactor(radio): show checked and unchecked for various states
    * feat(radio): implement `dense` prop and story
    * feat(radio): allow arbitrary content in label and add demo story
    * chore(radio): organise styles for better readability
    * fix(radio): increase icons size in dense mode for SVG padding
    * fix(radio): correct size of focus ring and input styles for cursor
    * fix(radio): remove story with Help component because it's out-of-scope
    * refactor(radio): use PureComponent instead of Component
    * chore(radio): remove Help from story because it's not related to Radio
    * refactor(radio): use inline SVG for icons in various styles
    * Revert "refactor(radio): use inline SVG for icons in various styles"
    * refactor(radio): style SVGs directly
    * chore(radio): remove redundant SVG icon from stories
    * refactor(radio): use updated SVGs that all use fill to simplify styles
    * feat(radio-group): introduce RadioGroup
    * fix(radio): implement improved SVG for dense mode
    * chore(radio): remove redundant dense class from icon
    * chore(radio-group): add stories
    * feat(radio-group-field): introduce RadioGroupField and stories
    * fix(fieldset): display as `inline-block` to reduce input width
    * refactor(radio): implement new SVG without padding and simplify styles
    * fix(radio): add white background layer to SVG
    * fix(radio-group-field): fix spacing
    * refactor(fieldset): remove inline-block from fieldset component
    
    BREAKING CHANGE: The `icon` and `required` props have been removed from the Radio component and the `value` and `label` props have become required.

--- src/Radio.js
+++ src/Radio.js
@@ -1,104 +1,28 @@
 import cx from 'classnames'
 import propTypes from '@dhis2/prop-types'
-import React, { Component, createRef } from 'react'
-import css from 'styled-jsx/css'
+import React, { PureComponent, createRef } from 'react'
 
 import { statusPropType } from './common-prop-types.js'
-import { Checked, Unchecked } from './icons/Radio.js'
+import { Regular, Dense } from './icons/Radio.js'
 import { colors, theme } from './theme.js'
-
-import styles from './Radio/styles.js'
-
-const icons = css.resolve`
-    svg {
-        display: block;
-        height: 24px;
-        width: 24px;
-        fill: ${theme.default};
-    }
-
-    .focus {
-        fill: ${colors.grey800};
-    }
-
-    .checked {
-        fill: ${colors.teal400};
-    }
-
-    .disabled {
-        color: ${theme.disabled};
-        fill: ${theme.disabled};
-    }
-
-    .error {
-        fill: ${theme.error};
-    }
-
-    .valid {
-        fill: ${theme.valid};
-    }
-
-    .warning {
-        fill: ${theme.warning};
-    }
-`
-
-const Input = React.forwardRef((props, ref) => (
-    <div>
-        <input type="radio" ref={ref} {...props} />
-
-        <style jsx>{`
-            div {
-                height: 0;
-                width: 0;
-                overflow: hidden;
-            }
-        `}</style>
-    </div>
-))
-Input.displayName = 'Input'
 
 /**
  * @module
  *
  * @param {Radio.PropTypes} props
- * @returns {React.Component}
+ * @returns {React.PureComponent}
  *
  * @example import { Radio } from '@dhis2/ui-core'
  *
  * @see Specification: {@link https://github.com/dhis2/design-system/blob/master/atoms/radio.md|Design system}
  * @see Live demo: {@link /demo/?path=/story/radio--default|Storybook}
  */
-class Radio extends Component {
+class Radio extends PureComponent {
     ref = createRef()
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            focus: props.initialFocus,
-        }
-    }
-
     componentDidMount() {
-        if (this.state.focus) {
+        if (this.props.initialFocus) {
             this.ref.current.focus()
-        }
-    }
-
-    onFocus = e => {
-        this.setState({ focus: true })
-
-        if (this.props.onFocus) {
-            this.props.onFocus(e)
-        }
-    }
-
-    onBlur = e => {
-        this.setState({ focus: false })
-
-        if (this.props.onBlur) {
-            this.props.onBlur(e)
         }
     }
 
@@ -108,34 +32,35 @@ class Radio extends Component {
             className,
             disabled,
             error,
-            icon,
             label,
             name,
             onChange,
-            required,
             tabIndex,
             valid,
             value,
             warning,
+            onFocus,
+            onBlur,
+            dense,
         } = this.props
-        const { focus } = this.state
 
-        const classes = cx(icons.className, {
-            checked: checked && !valid && !error && !warning,
+        const classes = cx({
+            checked,
             disabled,
             valid,
             error,
             warning,
-            focus,
         })
 
         return (
             <label
                 className={cx(className, {
                     disabled,
+                    dense,
                 })}
             >
-                <Input
+                <input
+                    type="radio"
                     ref={this.ref}
                     name={name}
                     value={value}
@@ -143,24 +68,73 @@ class Radio extends Component {
                     disabled={disabled}
                     tabIndex={tabIndex}
                     onChange={onChange}
-                    onFocus={this.onFocus}
-                    onBlur={this.onBlur}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
                 />
 
-                <div className={cx('icon', { focus })}>
-                    {checked ? (
-                        <Checked className={classes} />
+                <div className={cx('icon', { dense })}>
+                    {dense ? (
+                        <Dense className={classes} />
                     ) : (
-                        <Unchecked className={classes} />
+                        <Regular className={classes} />
                     )}
                 </div>
 
-                {icon}
+                {label}
 
-                <span className={cx({ required, disabled })}>{label}</span>
+                <style jsx>{`
+                    label {
+                        position: relative;
+                        display: flex;
+                        flex-direction: row;
+                        align-items: center;
+                        justify-content: flex-start;
+                        cursor: pointer;
+                        pointer-events: all;
+                        user-select: none;
+                        color: ${colors.grey900};
+                        padding-top: 4px;
+                        font-size: 16px;
+                        line-height: 20px;
+                    }
 
-                {icons.styles}
-                <style jsx>{styles}</style>
+                    label.dense {
+                        font-size: 14px;
+                        line-height: 16px;
+                        padding-top: 2px;
+                    }
+
+                    label.dense:first-of-type {
+                        padding-top: 4px;
+                    }
+
+                    label.disabled {
+                        cursor: not-allowed;
+                        color: ${theme.disabled};
+                    }
+                    input {
+                        position: absolute;
+                        opacity: 0;
+                        pointer-events: none;
+                    }
+
+                    .icon {
+                        pointer-events: none;
+                        user-select: none;
+                        margin-right: 5px;
+                        border: 2px solid transparent;
+                        padding: 1px;
+                        border-radius: 50%;
+                    }
+
+                    label.dense .icon {
+                        margin-right: 3px;
+                    }
+
+                    input:focus + .icon {
+                        border-color: ${colors.blue600};
+                    }
+                `}</style>
             </label>
         )
     }
@@ -169,15 +143,13 @@ class Radio extends Component {
 /**
  * @typedef {Object} PropTypes
  * @static
- * @prop {function} onChange
- * @prop {string} name
  * @prop {string} value
+ * @prop {Node} label
+ * @prop {function} [onChange]
+ * @prop {string} [name]
  * @prop {string} [className]
- * @prop {string} [label]
  * @prop {string} [tabIndex]
- * @prop {Element} [icon]
  *
- * @prop {boolean} [required]
  * @prop {boolean} [disabled]
  * @prop {boolean} [checked]
  * @prop {boolean} [initialFocus]
@@ -187,29 +159,31 @@ class Radio extends Component {
  * @prop {boolean} [warning]
  * @prop {boolean} [error]
  *
+ * @prop {boolean} [dense]
+ *
  * @prop {function} [onFocus]
  * @prop {function} [onBlur]
  */
 Radio.propTypes = {
-    onChange: propTypes.func.isRequired,
+    value: propTypes.string.isRequired,
+    label: propTypes.node.isRequired,
 
-    name: propTypes.string.isRequired,
-    value: propTypes.string,
+    name: propTypes.string,
     className: propTypes.string,
-    label: propTypes.string,
     tabIndex: propTypes.string,
 
-    icon: propTypes.element,
-
+    onChange: propTypes.func,
     onFocus: propTypes.func,
     onBlur: propTypes.func,
 
-    required: propTypes.bool,
     checked: propTypes.bool,
     disabled: propTypes.bool,
     valid: statusPropType,
     warning: statusPropType,
     error: statusPropType,
+
+    dense: propTypes.bool,
+
     initialFocus: propTypes.bool,
 }
 
