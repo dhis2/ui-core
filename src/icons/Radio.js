ab97e3143af6fb858e32ea23093920458e317853
Viktor Varland <varl@vlv.io>
2019-09-28 10:33:47 +0200

    refactor: implement the Radio component consistently with other inputs
    The Radio component now only contains a single radio-input with label, and supports a `dense` prop. A new RadioGroup component was introduced that contains a set of radios that toggle. The new RadioGroupField component contains a label, radio-group, help-text and validation-text, wrapped in a field.
    
    HISTORY:
    
    * refactor(radio): remove redundant styles
    * refactor(radio): remove focus state and style on input :focus selector
    * fix(radio): adjust disabled SVG fill color to match design-specs
    * fix(radio): remove stories that contain unsupported props
    * refactor(radio): move styles into component and remove Radio directory
    * refactor(radio): show checked and unchecked for various states
    * feat(radio): implement `dense` prop and story
    * feat(radio): allow arbitrary content in label and add demo story
    * chore(radio): organise styles for better readability
    * fix(radio): increase icons size in dense mode for SVG padding
    * fix(radio): correct size of focus ring and input styles for cursor
    * fix(radio): remove story with Help component because it's out-of-scope
    * refactor(radio): use PureComponent instead of Component
    * chore(radio): remove Help from story because it's not related to Radio
    * refactor(radio): use inline SVG for icons in various styles
    * Revert "refactor(radio): use inline SVG for icons in various styles"
    * refactor(radio): style SVGs directly
    * chore(radio): remove redundant SVG icon from stories
    * refactor(radio): use updated SVGs that all use fill to simplify styles
    * feat(radio-group): introduce RadioGroup
    * fix(radio): implement improved SVG for dense mode
    * chore(radio): remove redundant dense class from icon
    * chore(radio-group): add stories
    * feat(radio-group-field): introduce RadioGroupField and stories
    * fix(fieldset): display as `inline-block` to reduce input width
    * refactor(radio): implement new SVG without padding and simplify styles
    * fix(radio): add white background layer to SVG
    * fix(radio-group-field): fix spacing
    * refactor(fieldset): remove inline-block from fieldset component
    
    BREAKING CHANGE: The `icon` and `required` props have been removed from the Radio component and the `value` and `label` props have become required.

--- src/icons/Radio.js
+++ src/icons/Radio.js
@@ -1,50 +1,102 @@
 import React from 'react'
 import propTypes from '@dhis2/prop-types'
+import { colors, theme } from '../theme.js'
+import css from 'styled-jsx/css'
 
-export function Checked({ className }) {
+const styles = css`
+    svg {
+        display: block;
+        pointer-events: none;
+    }
+
+    svg {
+        fill: ${theme.default};
+    }
+
+    circle.background {
+        fill: ${colors.white};
+    }
+
+    svg.checked {
+        fill: ${colors.teal400};
+    }
+
+    svg.disabled {
+        fill: ${colors.grey400};
+    }
+
+    svg.error {
+        fill: ${theme.error};
+    }
+
+    svg.valid {
+        fill: ${theme.valid};
+    }
+
+    svg.warning {
+        fill: ${theme.warning};
+    }
+
+    svg:not(.checked) .inner,
+    svg:not(.checked) .outer.checked {
+        fill: none;
+    }
+`
+
+export function Regular({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
+            viewBox="0 0 18 18"
             className={className}
         >
-            <path d="M24 14c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm0-10C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 36c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z" />
+            <circle className="background" cx="9" cy="9" r="9"></circle>
+            <path
+                d="M9,18 C13.9705627,18 18,13.9705627 18,9 C18,4.02943725 13.9705627,0 9,0 C4.02943725,0 0,4.02943725 0,9 C0,13.9705627 4.02943725,18 9,18 Z M9,16 C5.13400675,16 2,12.8659932 2,9 C2,5.13400675 5.13400675,2 9,2 C12.8659932,2 16,5.13400675 16,9 C16,12.8659932 12.8659932,16 9,16 Z"
+                className="outer"
+            ></path>
+            <circle className="inner" cx="9" cy="9" r="5"></circle>
             <style jsx>{`
                 svg {
-                    fill: inherit;
-                    height: 24px;
-                    width: 24px;
-                    vertical-align: middle;
-                    pointer-events: none;
+                    height: 18px;
+                    width: 18px;
                 }
             `}</style>
+            <style jsx>{styles}</style>
         </svg>
     )
 }
-Checked.propTypes = {
+Regular.propTypes = {
     className: propTypes.string,
 }
 
-export function Unchecked({ className }) {
+export function Dense({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
+            viewBox="0 0 14 14"
             className={className}
         >
-            <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 36c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z" />
+            <circle className="background" cx="7" cy="7" r="7"></circle>
+            <path
+                d="M7,14 C10.8659932,14 14,10.8659932 14,7 C14,3.13400675 10.8659932,0 7,0 C3.13400675,0 0,3.13400675 0,7 C0,10.8659932 3.13400675,14 7,14 Z M7,13 C3.6862915,13 1,10.3137085 1,7 C1,3.6862915 3.6862915,1 7,1 C10.3137085,1 13,3.6862915 13,7 C13,10.3137085 10.3137085,13 7,13 Z"
+                className="outer unchecked"
+            ></path>
+            <path
+                d="M7,14 C10.8659932,14 14,10.8659932 14,7 C14,3.13400675 10.8659932,0 7,0 C3.13400675,0 0,3.13400675 0,7 C0,10.8659932 3.13400675,14 7,14 Z M7,12 C4.23857625,12 2,9.76142375 2,7 C2,4.23857625 4.23857625,2 7,2 C9.76142375,2 12,4.23857625 12,7 C12,9.76142375 9.76142375,12 7,12 Z"
+                className="outer checked"
+            ></path>
+            <circle className="inner" cx="7" cy="7" r="3"></circle>
             <style jsx>{`
                 svg {
-                    fill: inherit;
-                    height: 24px;
-                    width: 24px;
-                    vertical-align: middle;
-                    pointer-events: none;
+                    height: 14px;
+                    width: 14px;
                 }
             `}</style>
+            <style jsx>{styles}</style>
         </svg>
     )
 }
-Unchecked.propTypes = {
+Dense.propTypes = {
     className: propTypes.string,
 }
