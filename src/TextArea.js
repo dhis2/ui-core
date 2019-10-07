1ccbe2ac6cadfb346865f84689aeab587291f20a
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2019-10-07 09:58:45 +0200

    feat(textarea): introduce TextArea and TextAreaField with docs and stories (#456)
    * refactor: rename detectHorizontalScrollbarHeight to detectScrollbarSize
    Assuming that horizontal and vertical scrollbars are the same, this helper can be used more generically
    
    * feat(text-area): introduce TextArea component
    
    * refactor(textarea): renamed function for consistency
    
    * refactor(textarea): put styles in seperate file, refactor comp slightly
    
    * docs(textarea): add jsDocs to TextArea
    
    * feat(textarea): introduce TextAreaField incl jsDocs
    
    * docs(textarea): complete docs for all prop permutations
    
    * chore(status-icons): remove margin from status icons
    
    * feat(textarea): show icon outside of input field
    
    * docs(textarea): improve label text in resize story
    
    * docs(textarea): add doc to show textarea text overflow (scrollbar)
    
    * refactor(textarea): remove stale logic related to dynamic icon position
    
    * feat(textarea): add inputWidth to textAreaField
    
    * fix(textarea): process width string and number correctly for styled-jsx
    
    * docs(textarea): add inputWidth story
    
    * refactor(textarea): implement TextAreaField as a function, not a class
    
    * refactor(textarea): remove constructor
    
    * refactor(tabs): undo changes to detectHorizontalScrollbarHeight
    
    * refactor(textarea): remove redundant defaultProps
    
    * refactor(textarea): align class-names and css-selectors with Input
    
    * chore(textarea): remove redundant css rules
    
    * fix(textarea): map readOnly prop directly to readonly attribute
    
    * fix(textarea): disabled border color

---
+++ src/TextArea.js
@@ -0,0 +1,216 @@
+import propTypes from '@dhis2/prop-types'
+import React, { PureComponent } from 'react'
+import cx from 'classnames'
+
+import { statusPropType } from './common-prop-types.js'
+import { StatusIconNoDefault } from './icons/Status.js'
+
+import { styles } from './TextArea/styles.js'
+
+export class TextArea extends PureComponent {
+    textareaRef = React.createRef()
+    state = {
+        height: 'auto',
+    }
+    textareaDimensions = { width: 0, height: 0 }
+    userHasResized = false
+
+    componentDidMount() {
+        this.attachResizeListener()
+
+        if (this.props.initialFocus) {
+            this.textareaRef.current.focus()
+        }
+
+        if (this.shouldDoAutoGrow()) {
+            this.setHeight()
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.shouldDoAutoGrow()) {
+            this.setHeight()
+        }
+    }
+
+    attachResizeListener() {
+        const textarea = this.textareaRef.current
+        textarea.addEventListener('mousedown', this.setTextareaDimensions)
+        textarea.addEventListener('mouseup', this.hasUserResized)
+    }
+
+    removeResizeListener() {
+        const textarea = this.textareaRef.current
+        textarea.removeEventListener('mousedown', this.setTextareaDimensions)
+        textarea.removeEventListener('mouseup', this.hasUserResized)
+    }
+
+    setHeight() {
+        const textarea = this.textareaRef.current
+        const offset = textarea.offsetHeight - textarea.clientHeight
+        const height = textarea.scrollHeight + offset + 'px'
+        this.setState({ height })
+    }
+
+    setTextareaDimensions = () => {
+        const textarea = this.textareaRef.current
+        this.textareaDimensions = {
+            width: textarea.clientWidth,
+            height: textarea.clientHeight,
+        }
+    }
+
+    shouldDoAutoGrow() {
+        return this.props.autoGrow && !this.userHasResized
+    }
+
+    hasUserResized = () => {
+        const { width: oldWidth, height: oldHeight } = this.textareaDimensions
+
+        this.setTextareaDimensions()
+
+        const { width: newWidth, height: newHeight } = this.textareaDimensions
+        const userHasResized = newWidth !== oldWidth || newHeight !== oldHeight
+
+        if (userHasResized) {
+            this.userHasResized = true
+            this.removeResizeListener()
+        }
+    }
+
+    render() {
+        const {
+            className,
+            onChange,
+            onFocus,
+            onBlur,
+            dense,
+            disabled,
+            readOnly,
+            placeholder,
+            name,
+            valid,
+            error,
+            warning,
+            loading,
+            value,
+            tabIndex,
+            rows,
+            width,
+            resize,
+        } = this.props
+        const { height } = this.state
+        const textareaWidth = typeof width === 'number' ? `${width}px` : width
+
+        return (
+            <div className={cx('textarea', className)}>
+                <textarea
+                    id={name}
+                    name={name}
+                    placeholder={placeholder}
+                    ref={this.textareaRef}
+                    value={value}
+                    disabled={disabled}
+                    readOnly={readOnly}
+                    tabIndex={tabIndex}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    onChange={onChange}
+                    rows={rows}
+                    className={cx({
+                        dense,
+                        disabled,
+                        error,
+                        valid,
+                        warning,
+                        'read-only': readOnly,
+                    })}
+                />
+
+                <div className="status-icon">
+                    <StatusIconNoDefault
+                        error={error}
+                        valid={valid}
+                        loading={loading}
+                        warning={warning}
+                    />
+                </div>
+
+                <style jsx>{styles}</style>
+                <style jsx>{`
+                    textarea {
+                        width: ${textareaWidth};
+                        height: ${height};
+                        resize: ${resize};
+                    }
+                `}</style>
+            </div>
+        )
+    }
+}
+
+TextArea.defaultProps = {
+    rows: 4,
+    width: '100%',
+    resize: 'vertical',
+}
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ *
+ * @prop {string} name
+ * @prop {function} onChange
+ * @prop {function} [onBlur=() => {}]
+ * @prop {function} [onFocus=() => {}]
+ * @prop {string} [className]
+ * @prop {string} [placeholder]
+ * @prop {string} [value]
+ * @prop {string} [tabIndex]
+ *
+ * @prop {boolean} [disabled]
+ * @prop {boolean} [dense] - Compact mode
+ * @prop {boolean} [initialFocus]
+ *
+ * @prop {boolean} [valid] - `valid`, `warning`, `error`, and `loading`
+ * are mutually exclusive
+ * @prop {boolean} [warning]
+ * @prop {boolean} [error]
+ * @prop {boolean} [loading]
+ *
+ * @prop {boolean} [autoGrow]
+ * @prop {boolean} [readOnly]
+ * @prop {string} [resize=vertical] - rezise attribute on the textarea,
+ * one of `none`, `both`, `horizontal`, `vertical`
+ * @prop {number} [rows=4]
+ */
+
+TextArea.propTypes = {
+    name: propTypes.string.isRequired,
+
+    className: propTypes.string,
+
+    onChange: propTypes.func.isRequired,
+    onFocus: propTypes.func,
+    onBlur: propTypes.func,
+
+    value: propTypes.string,
+    placeholder: propTypes.string,
+    tabIndex: propTypes.string,
+
+    disabled: propTypes.bool,
+    readOnly: propTypes.bool,
+    dense: propTypes.bool,
+
+    valid: statusPropType,
+    warning: statusPropType,
+    error: statusPropType,
+    loading: propTypes.bool,
+
+    initialFocus: propTypes.bool,
+
+    rows: propTypes.number,
+    width: propTypes.oneOfType([propTypes.string, propTypes.number]),
+    resize: propTypes.oneOf(['none', 'both', 'horizontal', 'vertical']),
+    autoGrow: propTypes.bool,
+}
