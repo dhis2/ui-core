bf2eccb9310fe98e573793b2f5de6488cb49685e
ismay <ismay@ismaywolff.nl>
2019-10-08 12:32:04 +0200

    feat: add maxHeight prop

--- src/Select/SelectMenu.js
+++ src/Select/SelectMenu.js
@@ -1,29 +1,44 @@
 import React from 'react'
-import propTypes from '@dhis2/prop-types'
-
+import propTypes from 'prop-types'
+import { resolve } from 'styled-jsx/css'
 import { Card } from '../Card.js'
 import { spacers } from '../theme.js'
 import { SelectMenuList } from './SelectMenuList.js'
 
-const SelectMenu = ({ children, className }) => (
-    <div className={className}>
-        <Card>
-            <div className="select-menu-list-wrapper">
-                <SelectMenuList>{children}</SelectMenuList>
-            </div>
-        </Card>
+const SelectMenu = ({ children, maxHeight }) => {
+    // Override some Card styles to allow a max-height
+    const { styles, className } = resolve`
+        height: auto;
+        max-height: ${maxHeight};
+        overflow: auto;
+    `
 
-        <style jsx>{`
-            .select-menu-list-wrapper {
-                padding: ${spacers.dp4} 0;
-            }
-        `}</style>
-    </div>
-)
+    return (
+        <div>
+            <Card className={className}>
+                <div className="select-menu-list-wrapper">
+                    <SelectMenuList>{children}</SelectMenuList>
+                </div>
+            </Card>
+
+            {styles}
+
+            <style jsx>{`
+                .select-menu-list-wrapper {
+                    padding: ${spacers.dp4} 0;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+SelectMenu.defaultProps = {
+    maxHeight: '280px',
+}
 
 SelectMenu.propTypes = {
     children: SelectMenuList.propTypes.children,
-    className: propTypes.string,
+    maxHeight: propTypes.string,
 }
 
 export { SelectMenu }
