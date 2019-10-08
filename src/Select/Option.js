86e8643d1f02cadced4b7f321179e701a0631113
ismay <ismay@ismaywolff.nl>
2019-10-08 15:37:55 +0200

    refactor: simplify option component

--- src/Select/Option.js
+++ src/Select/Option.js
@@ -1,31 +1,53 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import cx from 'classnames'
-import styles from './Option.styles.js'
+import { colors, spacers } from '../theme.js'
 
-const createOnClickHandler = (onClick, option) => evt => {
-    evt.stopPropagation()
-    onClick(option)
-}
+const Option = ({ value, label, active, disabled, onClick }) => (
+    <a
+        className={cx({
+            disabled,
+            active,
+        })}
+        onClick={e => {
+            e.stopPropagation()
+            onClick({ value, label })
+        }}
+    >
+        {label}
 
-const Option = ({ value, label, active, disabled, onClick }) => {
-    const handleClicks = createOnClickHandler(onClick, { value, label })
+        <style jsx>{`
+            a {
+                display: block;
+                cursor: pointer;
+                font-size: 14px;
+                text-decoration: none;
+                color: ${colors.grey900};
+                padding: ${spacers.dp8} ${spacers.dp24};
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
 
-    return (
-        <li
-            className={cx({
-                disabled,
-                active,
-            })}
-        >
-            <a className="link" onClick={handleClicks}>
-                <div className="label">{label}</div>
-            </a>
+            a:hover {
+                background-color: ${colors.grey200};
+            }
 
-            <style jsx>{styles}</style>
-        </li>
-    )
-}
+            a:active,
+            a.active {
+                background-color: ${colors.grey400};
+                cursor: auto;
+            }
+
+            a.disabled {
+                color: rgba(0, 0, 0, 0.3);
+                cursor: not-allowed;
+                pointer-events: none;
+                user-select: none;
+            }
+        `}</style>
+    </a>
+)
 
 Option.propTypes = {
     label: propTypes.string.isRequired,
