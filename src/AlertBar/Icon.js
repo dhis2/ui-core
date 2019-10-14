7954da0661562661b2f75c59dde1a93a7f0330df
Hendrik de Graaf <hendrik@dhis2.org>
2019-10-14 17:04:36 +0200

    refactor(alert-bar): use StatusIcon to simplify component code

--- src/AlertBar/Icon.js
+++ src/AlertBar/Icon.js
@@ -1,32 +1,26 @@
 import propTypes from '@dhis2/prop-types'
 import React from 'react'
 
-import { statusPropType } from '../common-prop-types.js'
-
-import { Error, Info, Valid, Warning } from '../icons/Status.js'
+import { StatusIcon } from '../icons/Status.js'
 import { spacers } from '../theme.js'
 
-const Icon = ({ icon, success, warning, critical }) => {
+const Icon = ({ icon, success, warning, critical, info }) => {
     if (icon === false) {
         return null
     }
 
-    let IconComponent
-    if (React.isValidElement(icon)) {
-        IconComponent = icon
-    } else if (critical) {
-        IconComponent = <Error />
-    } else if (warning) {
-        IconComponent = <Warning />
-    } else if (success) {
-        IconComponent = <Valid />
-    } else {
-        IconComponent = <Info />
-    }
-
     return (
         <div>
-            {IconComponent}
+            {React.isValidElement(icon) ? (
+                icon
+            ) : (
+                <StatusIcon
+                    valid={success}
+                    error={critical}
+                    warning={warning}
+                    info={info}
+                />
+            )}
             <style jsx>{`
                 div {
                     margin-right: ${spacers.dp16};
@@ -41,12 +35,17 @@ const Icon = ({ icon, success, warning, critical }) => {
 }
 
 const iconPropType = propTypes.oneOfType([propTypes.bool, propTypes.element])
+const alertStatePropType = propTypes.mutuallyExclusive(
+    ['success', 'warning', 'critical', 'info'],
+    propTypes.bool
+)
 
 Icon.propTypes = {
     icon: iconPropType,
-    success: statusPropType,
-    warning: statusPropType,
-    critical: statusPropType,
+    success: alertStatePropType,
+    warning: alertStatePropType,
+    info: alertStatePropType,
+    critical: alertStatePropType,
 }
 
 export { Icon, iconPropType }
