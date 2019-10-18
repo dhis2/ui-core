9de64a94a4432df958b63f4d157670fd7d9b743e
Hendrik de Graaf <hendrik@dhis2.org>
2019-10-18 21:29:26 +0200

    refactor: toggle input components
    - refactored checkbox and switch API to be more inline with other inputs
    - switched to an SVG based approach for both checkbox and switch
    - Introduced various Toggle components that power the various exported Checkbox, Radio and Switch components
    - We are now exposing the following components:
        - Checkbox
        - CheckboxField
        - CheckboxGroup
        - CheckboxGroupField
        - Radio
        - RadioGroup
        - RadioGroupField
        - Switch
        - SwitchField
        - SwitchGroup
        - SwitchGroupField
    
    COMMIT HISTORY:
    * refactor: checkbox
        * implement Checkbox using the same approach as Radio
        * implement icon for regular and dense
        * remove references to removed icon
    * chore(checkbox): remove redundant files
    * chore(checkbox): use Component not PureComponent
    * fix(checkbox): correct border-radius for dense icon container
    * refactor(switch): align props and stories with Radio and Checkbox
    * refactor: replace RadioGroup(Field) by generic ToggleField(Group)
    * refactor: export Switch/Checkbox/Radio group components separately
    * fix: make value prop for checkbox and switch optional like in HTML specs
    * chore(switch): remove redundant styles file
    * fix(switch): set SVG width explicitly to prevent FF cutting off edge
    * fix(toggles): only add padding when in a group
    * feat: introduce CheckboxField and SwitchField with stories
    * refactor(toggle-field): implement as functional component
    * fix(toggle): correct prop-type toggleComponent which is a function
    * docs(toggles): add jsDoc to exported components and remove from internal
    
    BREAKING CHANGE: The `icon` and `required` props have been removed from the Checkbox and Switch. For Switch, the `label` prop has become required.

---
+++ src/CheckboxGroup.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import propTypes from '@dhis2/prop-types'
+
+import { ToggleGroup } from './ToggleGroup.js'
+import { Checkbox } from './Checkbox.js'
+
+/**
+ * @module
+ * @param {CheckboxGroup.PropTypes} props
+ * @returns {React.Component}
+ *
+ * @example import { CheckboxGroup } from '@dhis2/ui-core'
+ *
+ * @see Specification: {@link https://github.com/dhis2/design-system/blob/master/atoms/checkbox.md|Design system}
+ * @see Live demo: {@link /demo/?path=/story/checkboxgroup--default|Storybook}
+ */
+
+const CheckboxGroup = props => <ToggleGroup {...props} />
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ * @prop {Array.<Checkbox>} children
+ * @prop {function} onChange
+ * @prop {string} name
+ *
+ * @prop {Array.<String>} [value]
+ * @prop {string} [className]
+ *
+ * @prop {boolean} [disabled]
+ * @prop {boolean} [valid] - `valid`, `warning`, and `error` are
+ * mutually exclusive
+ * @prop {boolean} [warning]
+ * @prop {boolean} [error]
+ *
+ * @prop {boolean} [dense]
+ */
+CheckboxGroup.propTypes = {
+    ...ToggleGroup.propTypes,
+    children: propTypes.arrayOf(propTypes.instanceOfComponent(Checkbox))
+        .isRequired,
+    value: propTypes.arrayOf(propTypes.string),
+}
+
+export { CheckboxGroup }
