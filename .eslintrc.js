0995562219a831131cbb4bb117ee725eae2e932c
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-10-21 08:29:25 +0800

    chore: create cypress specific eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,4 @@ const { config } = require('@dhis2/cli-style')
 module.exports = {
     parser: 'babel-eslint',
     extends: ['eslint:recommended', 'plugin:react/recommended', config.eslint],
-    env: { es6: true },
-    globals: {
-        Cypress: 'readonly',
-        cy: 'readonly',
-    },
 }
