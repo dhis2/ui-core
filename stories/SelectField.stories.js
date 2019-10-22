b02d15799991683c5f0ccdf2b2a13d6f3c314902
ismay <ismay@ismaywolff.nl>
2019-10-22 15:05:22 +0200

    refactor: start reimplementing select field

---
+++ stories/SelectField.stories.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { SelectField, SelectOption } from '../src'
+
+const defaultProps = {
+    label: 'Default label',
+    selected: { value: '1', label: 'one' },
+    onChange: () => {},
+}
+
+const options = [
+    <SelectOption key="1" value="1" label="one" />,
+    <SelectOption key="2" value="2" label="two" />,
+]
+
+storiesOf('SelectField', module)
+    .add('Default', () => (
+        <SelectField {...defaultProps}>{options}</SelectField>
+    ))
+    .add('With Help text', () => (
+        <SelectField {...defaultProps} helpText="A helpful text.">
+            {options}
+        </SelectField>
+    ))
+    .add('With valid status', () => (
+        <SelectField
+            {...defaultProps}
+            helpText="A helpful text."
+            validationText="Totally valid"
+            valid
+        >
+            {options}
+        </SelectField>
+    ))
+    .add('With warning status', () => (
+        <SelectField
+            {...defaultProps}
+            helpText="A helpful text."
+            validationText="Hm, not quite, I warn thee!"
+            warning
+        >
+            {options}
+        </SelectField>
+    ))
+    .add('With error status', () => (
+        <SelectField
+            {...defaultProps}
+            helpText="A helpful text."
+            validationText="NO! TOTALLY WRONG!"
+            error
+        >
+            {options}
+        </SelectField>
+    ))
+    .add('Required', () => (
+        <SelectField {...defaultProps} required>
+            {options}
+        </SelectField>
+    ))
