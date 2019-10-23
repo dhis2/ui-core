156ecf12ac6c237045e3e34695b1332b89b31621
ismay <ismay@ismaywolff.nl>
2019-10-23 12:15:37 +0200

    docs: update stories

--- stories/SingleSelectField.stories.js
+++ stories/SingleSelectField.stories.js
@@ -5,17 +5,49 @@ import { SingleSelectField, SingleSelectOption } from '../src'
 const defaultProps = {
     label: 'Default label',
     selected: { value: '1', label: 'one' },
-    onChange: () => {},
+    onChange: ({ label }) => alert(`Selected changed to: ${label}`),
 }
 
 const options = [
     <SingleSelectOption key="1" value="1" label="one" />,
     <SingleSelectOption key="2" value="2" label="two" />,
+    <SingleSelectOption key="3" value="3" label="three" />,
+    <SingleSelectOption key="4" value="4" label="four" />,
+    <SingleSelectOption key="5" value="5" label="five" />,
+    <SingleSelectOption key="6" value="6" label="six" />,
+    <SingleSelectOption key="7" value="7" label="seven" />,
+    <SingleSelectOption key="8" value="8" label="eight" />,
+    <SingleSelectOption key="9" value="9" label="nine" />,
+    <SingleSelectOption key="10" value="10" label="ten" />,
 ]
 
 storiesOf('SingleSelectField', module)
     .add('Default', () => (
         <SingleSelectField {...defaultProps}>{options}</SingleSelectField>
+    ))
+    .add('No placeholder, no selected options', () => (
+        <SingleSelectField {...defaultProps} selected={{}}>
+            {options}
+        </SingleSelectField>
+    ))
+    .add('Placeholder, no selected options', () => (
+        <SingleSelectField
+            {...defaultProps}
+            placeholder="Placeholder text"
+            selected={{}}
+        >
+            {options}
+        </SingleSelectField>
+    ))
+    .add('Prefix, no selected options', () => (
+        <SingleSelectField {...defaultProps} prefix="Prefix text" selected={{}}>
+            {options}
+        </SingleSelectField>
+    ))
+    .add('Prefix, with selected options', () => (
+        <SingleSelectField {...defaultProps} prefix="Prefix text">
+            {options}
+        </SingleSelectField>
     ))
     .add('With Help text', () => (
         <SingleSelectField {...defaultProps} helpText="A helpful text.">
@@ -61,4 +93,26 @@ storiesOf('SingleSelectField', module)
         <SingleSelectField {...defaultProps} required>
             {options}
         </SingleSelectField>
+    ))
+    .add('Maximum height', () => (
+        <SingleSelectField {...defaultProps} maxHeight="100px">
+            {options}
+        </SingleSelectField>
+    ))
+    .add('Clearable', () => (
+        <SingleSelectField {...defaultProps} clearable>
+            {options}
+        </SingleSelectField>
+    ))
+    .add('Filtered', () => (
+        <SingleSelectField {...defaultProps} filtered>
+            {options}
+        </SingleSelectField>
+    ))
+    .add('Custom empty', () => (
+        <SingleSelectField
+            {...defaultProps}
+            selected={{}}
+            empty="There are no options"
+        />
     ))
