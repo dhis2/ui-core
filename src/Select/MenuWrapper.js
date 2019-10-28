7c76d502759ca2344f990522f127b974dbd44b63
ismay <ismay@ismaywolff.nl>
2019-10-28 17:46:14 +0100

    refactor: initial implementation to render select menu in portal
    This differs from the approach used in the DropMenu. I did not have
    the chance to check why the drop menu measures differently. Consider
    this an initial naieve implementation that I will verify and make
    more robust later on

--- src/Select/MenuWrapper.js
+++ src/Select/MenuWrapper.js
@@ -1,32 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import propTypes from 'prop-types'
 import { resolve } from 'styled-jsx/css'
 import { Card } from '../Card.js'
 import { layers } from '../theme.js'
 
-const MenuWrapper = ({ children, maxHeight, className }) => {
-    const { styles, className: cardClassName } = resolve`
-        height: auto;
-        max-height: ${maxHeight};
-        overflow: auto;
-    `
+class MenuWrapper extends Component {
+    state = {
+        top: 'auto',
+        left: 'auto',
+        width: 'auto',
+    }
 
-    return (
-        <div className={className}>
-            <Card className={cardClassName}>{children}</Card>
+    componentDidMount() {
+        window.addEventListener('resize', this.updatePosition)
+        this.updatePosition()
+    }
 
-            {styles}
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updatePosition)
+    }
 
-            <style jsx>{`
-                div {
-                    position: absolute;
-                    z-index: ${layers.applicationTop};
-                    left: 0;
-                    right: 0;
-                }
-            `}</style>
-        </div>
-    )
+    updatePosition = () => {
+        const selectEl = this.props.selectRef.current
+        const rect = selectEl.getBoundingClientRect()
+        const top = rect.bottom + window.scrollY
+        const left = rect.left + window.scrollX
+        const width = rect.width
+
+        const sizing = {
+            top: `${top}px`,
+            left: `${left}px`,
+            width: `${width}px`,
+        }
+
+        this.setState(sizing)
+    }
+
+    render() {
+        const { children, maxHeight, className } = this.props
+        const { top, left, width } = this.state
+        const { styles, className: cardClassName } = resolve`
+            height: auto;
+            max-height: ${maxHeight};
+            overflow: auto;
+        `
+
+        return ReactDOM.createPortal(
+            <div className={className}>
+                <Card className={cardClassName}>{children}</Card>
+
+                {styles}
+
+                <style jsx>{`
+                    div {
+                        position: absolute;
+                        z-index: ${layers.applicationTop};
+                        top: ${top};
+                        left: ${left};
+                        width: ${width};
+                    }
+                `}</style>
+            </div>,
+            document.body
+        )
+    }
 }
 
 MenuWrapper.defaultProps = {
@@ -37,6 +75,7 @@ MenuWrapper.propTypes = {
     className: propTypes.string,
     children: propTypes.node,
     maxHeight: propTypes.string,
+    selectRef: propTypes.object.isRequired,
 }
 
 export { MenuWrapper }
