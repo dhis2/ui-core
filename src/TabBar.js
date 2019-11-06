d62aaeb67e0d04690749ebee9c98daf19aa43fb8
Viktor Varland <varl@vlv.io>
2019-11-06 15:43:35 +0100

    refactor(tabbar): align component structure
    This refactors the TabBar to align better with the overal structure of
    the library.
    
    BREAKING CHANGE: This replaces the need to nest a TabBar in a ScrollBar,
    and instead allows `TabBar.propTypes.scrollable` to toggle if a TabBar
    is scrollable.
    
    BREAKING CHANGE: This deprecates external use of ScrollBar.

---
+++ src/TabBar.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import propTypes from '@dhis2/prop-types'
+import cx from 'classnames'
+
+import { colors } from './theme.js'
+import { Tab } from './Tab.js'
+import { ScrollBar } from './TabBar/ScrollBar.js'
+
+/**
+ * @module
+ * @param {TabBar.PropTypes} props
+ * @returns {React.Component}
+ *
+ * @example import { TabBar } from '@dhis2/ui-core'
+ *
+ * @see Specification: {@link https://github.com/dhis2/design-system/blob/master/molecules/tab.md|Design system}
+ * @see Live demo: {@link /demo/?path=/story/tabs--default-fluid|Storybook}
+ */
+const TabBar = ({ fixed, children, className, scrollable }) => {
+    const Container = scrollable ? ScrollBar : React.Fragment
+    return (
+        <Container>
+            <div className={cx('tab-bar', className, { fixed })}>
+                {children}
+
+                <style jsx>{`
+                    div {
+                        display: flex;
+                        align-items: flex-start;
+                        position: relative;
+                        overflow: hidden;
+                        box-shadow: inset 0 -1px 0 0 ${colors.grey400};
+                        flex-wrap: nowrap;
+                        flex-grow: 1;
+                        background: ${colors.white};
+                    }
+                `}</style>
+            </div>
+        </Container>
+    )
+}
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ * @prop {Tab|Array.<Tab>} children
+ * @prop {string} [className]
+ * @prop {boolean} [fixed]
+ * @prop {boolean} [scrollable]
+ */
+TabBar.propTypes = {
+    children: propTypes.oneOfType([
+        propTypes.instanceOfComponent(Tab),
+        propTypes.arrayOf(propTypes.instanceOfComponent(Tab)),
+    ]),
+    className: propTypes.string,
+    fixed: propTypes.bool,
+    scrollable: propTypes.bool,
+}
+
+export { TabBar }
