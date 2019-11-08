ff53deadc0c026ec31ac265ad545f868457f396f
Viktor Varland <varl@vlv.io>
2019-11-08 16:24:59 +0100

    refactor(checkbox): align 'on' function handlers
    BREAKING CHANGE: `onChange` is called with two parameters:
    First parameter an object with properties for `checked`, `name`, and
    `value`.
    The second is the React `event` object.

--- stories/Checkbox.stories.js
+++ stories/Checkbox.stories.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 import { Checkbox } from '../src'
 
-const logger = ({ target }) => console.info(`${target.name}: ${target.value}`)
+const logger = ({ name, value, checked }) =>
+    console.info(`name: ${name}, value: ${value}, checked: ${checked}`)
 
 storiesOf('Checkbox', module)
     // Regular
