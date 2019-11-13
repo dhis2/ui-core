2984429015993d23ed216b2c8250b8a4cb0459d9
Hendrik de Graaf <hendrik@dhis2.org>
2019-11-13 16:53:24 +0100

    fix(z-index-context): correct prop name

--- src/Select/MenuWrapper.js
+++ src/Select/MenuWrapper.js
@@ -5,6 +5,7 @@ import { resolve } from 'styled-jsx/css'
 import { Card } from '../Card.js'
 import { layers } from '../theme.js'
 
+<<<<<<< HEAD
 const MenuWrapper = ({
     children,
     maxHeight,
@@ -38,6 +39,88 @@ const MenuWrapper = ({
         </div>,
         document.body
     )
+=======
+class MenuWrapper extends Component {
+    state = {
+        top: 'auto',
+        left: 'auto',
+        width: 'auto',
+    }
+
+    requestId = null
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updatePosition)
+        this.updatePosition()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updatePosition)
+
+        if (this.requestId) {
+            window.cancelAnimationFrame(this.requestId)
+        }
+    }
+
+    updatePosition = () => {
+        const selectEl = this.props.selectRef.current
+
+        if (this.requestId) {
+            window.cancelAnimationFrame(this.requestId)
+        }
+
+        this.requestId = window.requestAnimationFrame(() => {
+            const rect = selectEl.getBoundingClientRect()
+            const top = rect.bottom + window.scrollY
+            const left = rect.left + window.scrollX
+            const width = rect.width
+
+            const sizing = {
+                top: `${top}px`,
+                left: `${left}px`,
+                width: `${width}px`,
+            }
+
+            this.setState(sizing)
+        })
+    }
+
+    render() {
+        const { children, maxHeight, className, menuRef, zIndex } = this.props
+        const { top, left, width } = this.state
+        const { styles, className: cardClassName } = resolve`
+            height: auto;
+            max-height: ${maxHeight};
+            overflow: auto;
+        `
+
+        return ReactDOM.createPortal(
+            <ZIndexConsumer
+                propZIndex={zIndex}
+                defaultZIndex={layers.applicationTop}
+            >
+                {computedZIndex => (
+                    <div className={className} ref={menuRef}>
+                        <Card className={cardClassName}>{children}</Card>
+
+                        {styles}
+
+                        <style jsx>{`
+                            div {
+                                position: absolute;
+                                z-index: ${computedZIndex};
+                                top: ${top};
+                                left: ${left};
+                                width: ${width};
+                            }
+                        `}</style>
+                    </div>
+                )}
+            </ZIndexConsumer>,
+            document.body
+        )
+    }
+>>>>>>> fix(z-index-context): correct prop name
 }
 
 MenuWrapper.defaultProps = {
