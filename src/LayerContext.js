830fe2b28676aabaaaf8123872b3688161bd5f05
Hendrik de Graaf <hendrik@dhis2.org>
2019-11-14 16:37:22 +0100

    refactor(layer-context): remove redundant fallback value

--- src/LayerContext.js
+++ src/LayerContext.js
@@ -4,7 +4,7 @@ const LayerContext = React.createContext(0)
 const LayerProvider = LayerContext.Provider
 
 const useLayerBasedZindex = (propZIndex = 0, defaultZIndex = 0) => {
-    const contextZIndex = useContext(LayerContext) || 0
+    const contextZIndex = useContext(LayerContext)
     return propZIndex || contextZIndex + defaultZIndex
 }
 
