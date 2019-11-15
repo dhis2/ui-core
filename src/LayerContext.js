a5c5530d06bb96967531dcd5ec688f4b8148fd94
Viktor Varland <varl@vlv.io>
2019-11-15 11:31:58 +0100

    refactor(layers): clean up implementation a bit

--- src/LayerContext.js
+++ src/LayerContext.js
@@ -1,11 +1,50 @@
 import React, { useContext } from 'react'
+import propTypes from '@dhis2/prop-types'
 
 const LayerContext = React.createContext(0)
-const LayerProvider = LayerContext.Provider
 
-const useLayerBasedZindex = (propZIndex = 0, defaultZIndex = 0) => {
-    const contextZIndex = useContext(LayerContext)
-    return propZIndex || contextZIndex + defaultZIndex
+const useLayer = (defaultLayer, customLayer) => {
+    const context = useContext(LayerContext)
+
+    if (customLayer) return customLayer
+
+    // stay within stack layer boundaries defined by the design system
+    // https://github.com/dhis2/design-system/blob/master/principles/spacing-alignment.md#stacking
+    const capReached = context && context % 1000 === 999
+    if (capReached) return context
+
+    if (context) return context + 1
+
+    return defaultLayer
 }
 
-export { LayerProvider, useLayerBasedZindex }
+/**
+ * @module
+ * @private
+ * @param {Layer.PropTypes} props
+ * @returns {React.Component}
+ */
+const Layer = ({ children, zIndex, zIndexBase }) => {
+    const newLayer = useLayer(zIndexBase, zIndex)
+
+    return (
+        <LayerContext.Provider value={newLayer}>
+            {children(newLayer)}
+        </LayerContext.Provider>
+    )
+}
+
+/**
+ * @typedef {Object} PropTypes
+ * @static
+ * @prop {Node} children
+ * @prop {number} zIndex
+ * @prop {number} zIndexBase
+ */
+Layer.propTypes = {
+    children: propTypes.node,
+    zIndexBase: propTypes.number,
+    zIndex: propTypes.number,
+}
+
+export { Layer, useLayer }
