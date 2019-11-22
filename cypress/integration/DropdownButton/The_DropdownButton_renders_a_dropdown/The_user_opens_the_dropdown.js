4854e63b930d727bf4d2d240703c91287abbe484
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-11-22 17:57:50 +0800

    chore(dropdown button): rename feature and step definition files

--- cypress/integration/DropdownButton/The_DropdownButton_renders_a_dropdown/The_user_opens_the_dropdown.js
+++
@@ -1,6 +0,0 @@
-import '../common/index'
-import { Then } from 'cypress-cucumber-preprocessor/steps'
-
-Then('the dropdown is rendered', () => {
-    cy.get('.dropdown-button-dropmenu').should('exist')
-})
