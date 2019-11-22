25da286f83d1aaa5f1ae2b714e14b5e06e9c1a7b
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-11-22 18:19:30 +0800

    chore(split button): rename feature and step definition files

--- cypress/integration/SplitButton/Arrow_opens_menu/index.js
+++ cypress/integration/SplitButton/Arrow_opens_menu/index.js
@@ -1,6 +1,10 @@
 import '../common/index'
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
 
+/**
+ * The user closes the DropMenu
+ * ============================
+ */
 Given('a SplitButton with opened DropMenu is rendered', () => {
     cy.visitStory('SplitButton: Basic', 'Default')
     cy.get('button:nth-child(2)').click()
@@ -9,4 +13,16 @@ Given('a SplitButton with opened DropMenu is rendered', () => {
 
 Then('the DropMenu is not rendered', () => {
     cy.get('.DropMenu-button-dropmenu').should('not.exist')
+})
+
+/**
+ * The user opens the DropMenu
+ * ===========================
+ */
+Given('a SplitButton with opened DropMenu is rendered', () => {
+    cy.visitStory('SplitButton: Basic', 'Default')
+})
+
+Then('the DropMenu is rendered', () => {
+    cy.get('.split-button-dropmenu').should('exist')
 })
