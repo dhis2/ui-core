0e2729aee8c74d859f38f2110efe847a6b5c12ac
ismay <ismay@ismaywolff.nl>
2019-11-28 12:31:49 +0100

    test(select): add select field help text test

--- stories/MultiSelectField.stories.testing.js
+++ stories/MultiSelectField.stories.testing.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { MultiSelectField, MultiSelectOption } from '../src'
 
-storiesOf('MultiSelectField', module).add('With label', () => (
-    <MultiSelectField label="The label">
-        <MultiSelectOption value="1" label="one" />,
-        <MultiSelectOption value="2" label="two" />,
-        <MultiSelectOption value="3" label="three" />,
-    </MultiSelectField>
-))
+storiesOf('MultiSelectField', module)
+    .add('With label', () => (
+        <MultiSelectField label="The label">
+            <MultiSelectOption value="1" label="one" />,
+            <MultiSelectOption value="2" label="two" />,
+            <MultiSelectOption value="3" label="three" />,
+        </MultiSelectField>
+    ))
+    .add('With help text', () => (
+        <MultiSelectField helpText="The help text">
+            <MultiSelectOption value="1" label="one" />,
+            <MultiSelectOption value="2" label="two" />,
+            <MultiSelectOption value="3" label="three" />,
+        </MultiSelectField>
+    ))
--- stories/SingleSelectField.stories.testing.js
+++ stories/SingleSelectField.stories.testing.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { SingleSelectField, SingleSelectOption } from '../src'
 
-storiesOf('SingleSelectField', module).add('With label', () => (
-    <SingleSelectField label="The label">
-        <SingleSelectOption value="1" label="one" />,
-        <SingleSelectOption value="2" label="two" />,
-        <SingleSelectOption value="3" label="three" />,
-    </SingleSelectField>
-))
+storiesOf('SingleSelectField', module)
+    .add('With label', () => (
+        <SingleSelectField label="The label">
+            <SingleSelectOption value="1" label="one" />,
+            <SingleSelectOption value="2" label="two" />,
+            <SingleSelectOption value="3" label="three" />,
+        </SingleSelectField>
+    ))
+    .add('With help text', () => (
+        <SingleSelectField helpText="The help text">
+            <SingleSelectOption value="1" label="one" />,
+            <SingleSelectOption value="2" label="two" />,
+            <SingleSelectOption value="3" label="three" />,
+        </SingleSelectField>
+    ))
