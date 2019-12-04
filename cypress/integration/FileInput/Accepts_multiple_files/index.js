1a1af2024d63a5889a5e037fb7c2445bb81fa0b4
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-12-04 11:07:54 +0700

    chore(file input test): remove adding files scenario

--- cypress/integration/FileInput/Accepts_multiple_files/index.js
+++ cypress/integration/FileInput/Accepts_multiple_files/index.js
@@ -5,13 +5,6 @@ Given('a FileInput that accepts multiple files is rendered', () => {
     cy.visitStory('FileInput', 'Multiple')
 })
 
-Given('the FileInput has some files attached', () => {
-    cy.get('.file-input input').uploadMultipleFiles([
-        { fileType: 'foo', fixture: 'FileInput/file.foo' },
-        { fileType: 'bar', fixture: 'FileInput/file.bar' },
-    ])
-})
-
 When('the user selected multiple files', () => {
     cy.get('.file-input input').uploadMultipleFiles(
         [
@@ -32,24 +25,5 @@ Then("the onChange handler's payload contains multiple files", () => {
 
         const file2 = files[1]
         expect(file2.name).to.equal('file.txt')
-    })
-})
-
-Then("the onChange handler's payload contains all files", () => {
-    cy.get('@payload').then(payload => {
-        const files = payload.files
-        expect(files).to.have.lengthOf(4)
-
-        const file1 = files[0]
-        expect(file1.name).to.equal('file.foo')
-
-        const file2 = files[1]
-        expect(file2.name).to.equal('file.bar')
-
-        const file3 = files[2]
-        expect(file3.name).to.equal('file.md')
-
-        const file4 = files[3]
-        expect(file4.name).to.equal('file.txt')
     })
 })
