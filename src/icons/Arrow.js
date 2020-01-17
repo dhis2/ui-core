02cb54662b6d397decccf483190ca59775522634
joe-cooper <33054985+cooper-joe@users.noreply.github.com>
2020-01-17 09:52:26 +0100

    fix(button): adjust arrow icon used in buttons
    fix: change dropdown arrow icon and remove directional switching
    
    fix: dropdown arrow directional switching
    
    fix: arrow icon SVG formatting

--- src/icons/Arrow.js
+++ src/icons/Arrow.js
@@ -5,15 +5,15 @@ export function ArrowDown({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
+            viewBox="0 0 12 12"
             className={className}
         >
-            <path d="M14 20l10 10 10-10z" />
+            <path d="m5.29289 8.7071c.39053.3905 1.02369.3905 1.41422 0l2.99999-2.99999c.3905-.39053.3905-1.02369 0-1.41422-.3905-.39052-1.0237-.39052-1.4142 0l-2.2929 2.2929-2.29289-2.2929c-.39053-.39052-1.02369-.39052-1.41422 0-.39052.39053-.39052 1.02369 0 1.41422z" />
             <style jsx>{`
                 svg {
                     fill: inherit;
-                    height: 24px;
-                    width: 24px;
+                    height: 12px;
+                    width: 12px;
                     vertical-align: middle;
                     pointer-events: none;
                 }
@@ -29,15 +29,18 @@ export function ArrowUp({ className }) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
+            viewBox="0 0 12 12"
             className={className}
         >
-            <path d="M14 28l10-10 10 10z" />
+            <path
+                d="m5.29289 8.7071c.39053.3905 1.02369.3905 1.41422 0l2.99999-2.99999c.3905-.39053.3905-1.02369 0-1.41422-.3905-.39052-1.0237-.39052-1.4142 0l-2.2929 2.2929-2.29289-2.2929c-.39053-.39052-1.02369-.39052-1.41422 0-.39052.39053-.39052 1.02369 0 1.41422z"
+                transform="matrix(1 0 0 -1 0 12.999974)"
+            />
             <style jsx>{`
                 svg {
                     fill: inherit;
-                    height: 24px;
-                    width: 24px;
+                    height: 12px;
+                    width: 12px;
                     vertical-align: middle;
                     pointer-events: none;
                 }
