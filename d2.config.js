e0a150a7a712dd58a3e7403b0f1668c68a976681
Viktor Varland <varl@vlv.io>
2020-01-17 16:21:44 +0100

    chore: migrate to use d2-app-scripts to build
    This removes the need to have custom configuration for e.g. Babel,
    instead relying on the DHIS2 default configurations that are setup.

--- d2.config.js
+++ d2.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    type: 'lib',
+    entryPoint: {
+        lib: 'src/index.js',
+    },
     docsite: {
         name: '@dhis2/ui-core',
         description: 'Component primitives for DHIS2 user interfaces',
