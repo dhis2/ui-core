20a5517e394f4ed5428d821217ea0d59499dce19
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-01-22 09:27:25 +0100

    feat(node): add content, label & leaves data-test props

--- src/Node/Leaves.js
+++ src/Node/Leaves.js
@@ -2,8 +2,8 @@ import React from 'react'
 import propTypes from '@dhis2/prop-types'
 import cx from 'classnames'
 
-export const Contents = ({ children, open }) => (
-    <div className={cx({ open })}>
+export const Leaves = ({ children, open, dataTest }) => (
+    <div className={cx({ open })} data-test={dataTest}>
         {children}
 
         <style jsx>{`
@@ -20,7 +20,8 @@ export const Contents = ({ children, open }) => (
     </div>
 )
 
-Contents.propTypes = {
+Leaves.propTypes = {
     children: propTypes.node,
+    dataTest: propTypes.string,
     open: propTypes.bool,
 }
