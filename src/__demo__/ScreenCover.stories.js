a7f8612bc3df6b2cf92c2423ce570de0eab555ce
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-01-31 16:05:49 +0100

    docs(screen cover): add alignment stories

--- src/__demo__/ScreenCover.stories.js
+++ src/__demo__/ScreenCover.stories.js
@@ -24,8 +24,92 @@ storiesOf('ScreenCover', module)
         </ScreenCover>
     ))
 
-    .add('Menu', () => (
+    .add('Align top: Menu', () => (
         <ScreenCover onClick={onClick}>
+            <Menu>
+                <MenuItem label="One" value="one">
+                    <Menu>
+                        <MenuItem label="Sub One" value="subone">
+                            <Menu>
+                                <MenuItem label="Sub One-One" value="suboneone">
+                                    <Menu>
+                                        <MenuItem
+                                            label="Sub One-One-One"
+                                            value="suboneoneone"
+                                            onClick={val => {
+                                                alert(`this is ${val}`)
+                                            }}
+                                        />
+                                    </Menu>
+                                </MenuItem>
+                            </Menu>
+                        </MenuItem>
+                    </Menu>
+                </MenuItem>
+
+                <Divider />
+                <MenuItem
+                    label="Two"
+                    value="two"
+                    onClick={val => {
+                        alert(`this is ${val}`)
+                    }}
+                />
+                <MenuItem
+                    label="Three"
+                    value="three"
+                    onClick={val => {
+                        alert(`this is ${val}`)
+                    }}
+                />
+            </Menu>
+        </ScreenCover>
+    ))
+
+    .add('Align middle: Menu', () => (
+        <ScreenCover onClick={onClick} alignMiddle>
+            <Menu>
+                <MenuItem label="One" value="one">
+                    <Menu>
+                        <MenuItem label="Sub One" value="subone">
+                            <Menu>
+                                <MenuItem label="Sub One-One" value="suboneone">
+                                    <Menu>
+                                        <MenuItem
+                                            label="Sub One-One-One"
+                                            value="suboneoneone"
+                                            onClick={val => {
+                                                alert(`this is ${val}`)
+                                            }}
+                                        />
+                                    </Menu>
+                                </MenuItem>
+                            </Menu>
+                        </MenuItem>
+                    </Menu>
+                </MenuItem>
+
+                <Divider />
+                <MenuItem
+                    label="Two"
+                    value="two"
+                    onClick={val => {
+                        alert(`this is ${val}`)
+                    }}
+                />
+                <MenuItem
+                    label="Three"
+                    value="three"
+                    onClick={val => {
+                        alert(`this is ${val}`)
+                    }}
+                />
+            </Menu>
+        </ScreenCover>
+    ))
+
+    .add('Align bottom: Menu', () => (
+        <ScreenCover onClick={onClick} alignBottom>
             <Menu>
                 <MenuItem label="One" value="one">
                     <Menu>
