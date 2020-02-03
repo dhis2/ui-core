1eb0ac1af1a8497fe23f0b936237ce4f0d862872
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-02-03 16:07:37 +0100

    fix(node): use previously replace arrow icon

--- src/Node/Arrow.js
+++ src/Node/Arrow.js
@@ -2,8 +2,22 @@ import React from 'react'
 import propTypes from '@dhis2/prop-types'
 import cx from 'classnames'
 
-import { ArrowDown } from '../icons/Arrow.js'
 import { colors } from '../theme.js'
+
+const ArrowDown = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+        <path d="M14 20l10 10 10-10z" />
+        <style jsx>{`
+            svg {
+                fill: inherit;
+                height: 24px;
+                width: 24px;
+                vertical-align: middle;
+                pointer-events: none;
+            }
+        `}</style>
+    </svg>
+)
 
 export const Arrow = ({ open, onOpen, onClose, dataTest }) => {
     const onClick = open ? onClose : onOpen
