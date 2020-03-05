b44393e196f28cfa3946f11125f625659ea2231b
Viktor Varland <varl@vlv.io>
2020-03-05 16:44:03 +0100

    refactor(loaders): colocate code and stories

--- packages/core/src/LinearLoader/LinearLoader.js
+++ packages/core/src/LinearLoader/LinearLoader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import propTypes from '@dhis2/prop-types'
-import { theme, spacers } from './theme.js'
+import { theme, spacers } from '../theme.js'
 
 const Progress = ({ amount }) => {
     return (
