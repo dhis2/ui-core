9f651a2a8e257ddbad7cbbea94a596ba24b88c77
joe-cooper <33054985+cooper-joe@users.noreply.github.com>
2020-03-13 15:35:43 +0100

    fix(switch): adjust letter spacing

--- src/Switch.js
+++ src/Switch.js
@@ -116,11 +116,13 @@ class Switch extends Component {
                         user-select: none;
                         color: ${colors.grey900};
                         font-size: 16px;
+                        letter-spacing: 0.3px;
                         line-height: 20px;
                     }
 
                     label.dense {
                         font-size: 14px;
+                        letter-spacing: 0.2px;
                         line-height: 16px;
                     }
 
